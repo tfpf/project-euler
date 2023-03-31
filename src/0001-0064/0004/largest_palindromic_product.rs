98418548cdaa21d50852cde1654546b3470c1194
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-03-31 20:51:34 +0530

    Problem 4.

---
+++ src/0001-0064/0004/largest_palindromic_product.rs
@@ -0,0 +1,26 @@
+/// Check whether a number is a palindrome or not.
+///
+/// * `num` - Number to check.
+fn is_palindrome(num: i32) -> bool
+{
+    let num = num.to_string();
+    num == num.chars().rev().collect::<String>()
+}
+
+/// Main function.
+fn main()
+{
+    let mut result = -1;
+    for i in (100..1000).rev()
+    {
+        for j in (100..1000).rev()
+        {
+            let product = i * j;
+            if product > result && is_palindrome(product)
+            {
+                result = product;
+            }
+        }
+    }
+    println!("{result}");
+}
