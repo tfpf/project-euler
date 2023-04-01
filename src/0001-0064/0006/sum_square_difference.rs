613c14592bd4ceec01626329153ebf1e1a3695e4
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-04-01 15:10:36 +0530

    Solution 6.

---
+++ src/0001-0064/0006/sum_square_difference.rs
@@ -0,0 +1,10 @@
+/// Main function.
+fn main()
+{
+    // I could apply the formulae to calculate the sums, but then I wouldn't
+    // be learning Rust.
+    let square_of_sum = (1..101).sum::<i32>().pow(2);
+    let sum_of_squares = (1..101).fold(0, |s, elem| s + elem * elem);
+    let difference = square_of_sum - sum_of_squares;
+    println!("{difference}");
+}
