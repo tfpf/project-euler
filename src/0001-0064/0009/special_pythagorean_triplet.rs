c0f15616ac7e2e01a94c48aa4b55f3430705452f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-04-01 23:35:02 +0530

    Solution 9 and explanation.

---
+++ src/0001-0064/0009/special_pythagorean_triplet.rs
@@ -0,0 +1,32 @@
+/// Find the product of the only Pythagorean triplet with sum 1000.
+fn find_triplet() -> i32
+{
+    for a in 1..334
+    {
+        for c in 334..998
+        {
+            let b = 1000 - a - c;
+            if a >= b || b >= c
+            {
+                continue;
+            }
+            if b * b == c * c - a * a
+            {
+                return a * b * c;
+            }
+        }
+    }
+
+    // Shouldn't happen, since we are told that a triplet satisfying the given
+    // condition exists.
+    return -1;
+}
+
+/// Main function.
+fn main()
+{
+    let result = find_triplet();
+    println!("{}", result);
+
+    assert_eq!(result, 31875000);
+}
