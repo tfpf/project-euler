eae4b0f98591e2a13811639980d3cd47c4872f0e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-04-16 21:40:44 +0530

    Solution 20 done.
    Implementation of addition of `BigNum` objects is not used. However, I will leave it around in acse I need it later.

--- src/0001-0100/0020/factorial_digit_sum.rs
+++ src/0001-0100/0020/factorial_digit_sum.rs
@@ -91,8 +91,81 @@ impl std::ops::Add for BigNum
     }
 }
 
+/// Multiplication.
+impl std::ops::Mul<u32> for BigNum
+{
+    type Output = BigNum;
+    fn mul(self, other: u32) -> BigNum
+    {
+        let mut digits: Vec<u32> = vec![];
+        let mut carry: u32 = 0;
+        for digit in self.digits
+        {
+            let product: u64 = digit as u64 * other as u64 + carry as u64;
+            let product: u32 = if product >= 1_000_000_000
+            {
+                carry = (product / 1_000_000_000) as u32;
+                product % 1_000_000_000
+            }
+            else
+            {
+                carry = 0;
+                product
+            } as u32;
+            digits.push(product);
+        }
+        if carry > 0
+        {
+            digits.push(carry);
+        }
+        BigNum
+        {
+            digits: digits,
+        }
+    }
+}
+
+/// Calculate the factorial of a number.
+///
+/// * `num`
+///
+/// -> Factorial of `num`.
+fn factorial(num: u32) -> BigNum
+{
+    let mut product = BigNum::new(1);
+    match num
+    {
+        0 | 1 => return product,
+        _ => (),
+    };
+
+    for i in 2..=num
+    {
+        product = product * i;
+    }
+    product
+}
+
+/// Add up the digits of a number.
+///
+/// * `num`
+///
+/// -> Sum of the digits of `num`.
+fn digit_sum(num: u32) -> u32
+{
+    if num < 10
+    {
+        return num;
+    }
+    num % 10 + digit_sum(num / 10)
+}
+
 /// Main function.
 fn main()
 {
-    let result = BigNum::new(1);
+    let f = factorial(100);
+    let result: u32 = f.digits.iter().map(|&d| digit_sum(d)).sum();
+    println!("{}", result);
+
+    assert_eq!(result, 648);
 }
