19f4ca63f2e36daa055215f12efd61fe9bfdbb65
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-10-29 13:54:21 +0530

    Rustified problem 2 even more.

--- src/solutions/even_fibonacci_numbers.rs
+++ src/solutions/even_fibonacci_numbers.rs
@@ -1,11 +1,9 @@
 use crate::utils;
 
 pub fn solve() {
-    let fibonacci = utils::Fibonacci::new(1, 2);
-    let sum: i64 = fibonacci
-        .filter(|num| *num % 2 == 0)
-        .take_while(|num| *num < 4000000)
-        .sum();
+    // Since we need only even terms, start from 2 and take every third term.
+    let fibonacci = utils::Fibonacci::new(2, 3);
+    let sum: i64 = fibonacci.step_by(3).take_while(|num| *num < 4000000).sum();
 
     println!("{}", sum);
     assert_eq!(sum, 4613732);
