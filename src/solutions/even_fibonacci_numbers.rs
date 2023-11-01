7e913e44c37a66ce5aeca21b75a35787b668e5de
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-01 18:13:33 +0530

    Ran `rustfmt`.

--- src/solutions/even_fibonacci_numbers.rs
+++ src/solutions/even_fibonacci_numbers.rs
@@ -3,10 +3,7 @@ use crate::utils;
 pub fn solve() {
     // Since we need only even terms, start from 2 and take every third term.
     let fibonacci = utils::Fibonacci::new(utils::Long::new("2"), utils::Long::new("3"));
-    let sum: utils::Long = fibonacci
-        .step_by(3)
-        .take_while(|num| num < &4000000)
-        .sum();
+    let sum: utils::Long = fibonacci.step_by(3).take_while(|num| num < &4000000).sum();
     let sum = sum.get();
 
     println!("{}", sum);
