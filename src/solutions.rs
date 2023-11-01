2d4f02f42537ff44dbf01ef5ca1668ca6bb5be38
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-01 15:08:21 +0530

    Ran `rustfmt`.

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,5 +1,6 @@
 pub mod even_fibonacci_numbers;
 pub mod highly_divisible_triangular_number;
+pub mod large_sum;
 pub mod largest_palindromic_product;
 pub mod largest_prime_factor;
 pub mod largest_product_in_a_grid;
@@ -10,4 +11,3 @@ pub mod special_pythagorean_triplet;
 pub mod sum_square_difference;
 pub mod summation_of_primes;
 pub mod ten_thousand_and_first_prime;
-pub mod large_sum;
