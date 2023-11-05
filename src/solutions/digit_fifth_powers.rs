43b632328436c73d338f97060c06cc42c8b5b252
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-05 13:49:35 +0530

    Added brute-force solution.

--- src/solutions/digit_fifth_powers.rs
+++ src/solutions/digit_fifth_powers.rs
@@ -1,14 +1,14 @@
 pub fn solve() {
-    let nums = (1i32..999999)
+    let sum: u32 = (10..10000000)
         .filter(|&num| {
-            let mut n = num;
-            let mut sum = 0;
-            while num > 0 {
-                sum += (n % 10).pow(5);
-                n /= 10;
-            }
-            sum == num
+            num.to_string()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap().pow(5))
+                .sum::<u32>()
+                == num
         })
-        .collect::<Vec<i32>>();
-    println!("{:?}", nums);
+        .sum();
+
+    println!("{}", sum);
+    assert_eq!(sum, 443839);
 }
