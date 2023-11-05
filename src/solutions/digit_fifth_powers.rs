977f14ba6e2d158b5c00e0125d8bc45941c29650
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-05 13:54:31 +0530

    Optimisaion using arithmetic instead of stringification.

--- src/solutions/digit_fifth_powers.rs
+++ src/solutions/digit_fifth_powers.rs
@@ -1,11 +1,13 @@
 pub fn solve() {
-    let sum: u32 = (10..10000000)
+    let sum: i32 = (10i32..10000000)
         .filter(|&num| {
-            num.to_string()
-                .chars()
-                .map(|c| c.to_digit(10).unwrap().pow(5))
-                .sum::<u32>()
-                == num
+            let mut n = num;
+            let mut val = 0;
+            while n > 0 {
+                val += (n % 10).pow(5);
+                n /= 10;
+            }
+            val == num
         })
         .sum();
 
