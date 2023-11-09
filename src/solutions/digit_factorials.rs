2dee3bcddda49cb7098624c276e27a908e3d38f9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-09 23:24:39 +0530

    Range optimisation in P34.

--- src/solutions/digit_factorials.rs
+++ src/solutions/digit_factorials.rs
@@ -5,8 +5,13 @@ pub fn solve() {
     let factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
 
     // An easy upper bound is 9999999, which exceeds the sum of the factorials
-    // of its digits.
-    let sum: i64 = (10..10000000)
+    // of its digits. However, a better upper bound is the ordinate of the
+    // point at which the graphs of
+    //     y = x (value of a number)
+    // and
+    //     y = 362880 × log(x, 10) (maximum sum of its digit factorials)
+    // intersect.
+    let sum: i64 = (10..2309171)
         .filter(|&num| {
             utils::Digits::new(num)
                 .map(|digit| factorial[digit as usize])
