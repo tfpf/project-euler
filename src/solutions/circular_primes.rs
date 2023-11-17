0bd49cc383b7e4bb53057a41608f126f2f73146f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-17 19:49:53 +0530

    Solved.

--- src/solutions/circular_primes.rs
+++ src/solutions/circular_primes.rs
@@ -1,1 +1,33 @@
-pub fn solve() {}
+use crate::utils;
+
+/// Check whether the given number is a circular prime number.
+///
+/// * `num` - Prime number.
+/// * `prime` - Sieve of eratosthenes.
+///
+/// -> Whether the number is a circular prime.
+fn is_circular_prime(mut num: i64, prime: &Vec<bool>) -> bool {
+    // Since the number is prime, only its rotations have to be checked.
+    let passes = utils::Digits::new(num).count() - 1;
+    for _ in 0..passes {
+        num = num / 10 + num % 10 * 10i64.pow(passes as u32);
+        if !prime[num as usize] {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn solve() {
+    let prime = utils::sieve_of_eratosthenes(1000000);
+    let result = prime
+        .iter()
+        .enumerate()
+        .filter(|(_, is_prime)| **is_prime)
+        .map(|(num, _)| num as i64)
+        .filter(|&num| is_circular_prime(num, &prime))
+        .count();
+
+    println!("{}", result);
+    assert_eq!(result, 55);
+}
