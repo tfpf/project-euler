3e97a350819f4205a9efa4fa018e865b1c216ed1
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-24 19:52:09 +0530

    No need to check all primes if exceeded.

--- src/solutions/non_abundant_sums.rs
+++ src/solutions/non_abundant_sums.rs
@@ -1,20 +1,25 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let prime_divisors = utils::PrimeDivisors::new(28124);
-    let mut abundant_numbers = vec![];
-    for num in 12..28124 {
-        // If the prime factorisation of a number is known, the sum of its
-        // divisors is easily calculated using a formula. This block implements
-        // said formula.
-        let sum_of_divisors: i64 = prime_divisors
-            .iter(num)
-            .map(|(prime, power)| (prime.pow(power + 1) - 1) / (prime - 1))
-            .product();
-        if sum_of_divisors > num + num {
-            abundant_numbers.push(num);
+    let primes = utils::Primes::new(28124).iter().collect::<Vec<i64>>();
+    let abundant_numbers = (12..28124).filter(|&num| {
+        let mut num = num;
+        let sum_of_divisors_lb = num + num;
+        let mut sum_of_divisors = 1;
+        for &prime in &primes {
+            if prime > num {
+                break;
+            }
+            let mut power = 0;
+            while num % prime == 0 {
+                num /= prime;
+                power += 1;
+            }
+            sum_of_divisors *= (prime.pow(power + 1) - 1) / (prime - 1);
         }
-    }
+        sum_of_divisors > sum_of_divisors_lb
+    }).collect::<Vec<i64>>();
+
     let sum = (0..28124)
         .filter(|&num| {
             // Can this number be expressed as the sum of two abundant numbers?
