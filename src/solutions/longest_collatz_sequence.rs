f182e8798cc2027ef4f4ad6060f2223ea5a5ebaa
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-24 12:55:55 +0530

    Optimised P14 with dynamic programming. (#30)

--- src/solutions/longest_collatz_sequence.rs
+++ src/solutions/longest_collatz_sequence.rs
@@ -1,15 +1,33 @@
-use crate::utils;
+/// Find the length of the Collatz sequence starting from the given number
+/// and ending at 1.
+///
+/// * `collatz_lengths` - Array to cache the lengths.
+/// * `num` - Number to find the length of the Collatz sequence for.
+///
+/// -> Length of Collatz sequence.
+fn get_collatz_length(collatz_lengths: &mut Vec<i32>, num: usize) -> i32 {
+    if num == 1 {
+        return 1;
+    }
+    if num < collatz_lengths.len() && collatz_lengths[num] != 0 {
+        return collatz_lengths[num];
+    }
+    let num_next = if num & 1 == 0 { num >> 1 } else { 3 * num + 1 };
+    let len = 1 + get_collatz_length(collatz_lengths, num_next);
+    if num < collatz_lengths.len() {
+        collatz_lengths[num] = len;
+    }
+    len
+}
 
 pub fn solve() -> i64 {
-    let result = (1..1_000_000)
-        .map(|num| {
-            let collatz = utils::Collatz::new(num);
-            (collatz.count(), num)
-        })
+    let mut collatz_lengths = vec![0; 1000000];
+    let result = (2..collatz_lengths.len())
+        .map(|num| (get_collatz_length(&mut collatz_lengths, num), num))
         .max()
         .unwrap()
         .1;
 
     assert_eq!(result, 837799);
-    result
+    result as i64
 }
