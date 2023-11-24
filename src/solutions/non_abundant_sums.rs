dfdb0bdd5932f60fe3633f25fe3608b66471d6ea
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-24 15:44:53 +0530

    Sorted vector instead of B-tree in P23.
    Saves about 20 ms.

--- src/solutions/non_abundant_sums.rs
+++ src/solutions/non_abundant_sums.rs
@@ -1,20 +1,21 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let abundant_numbers = std::collections::BTreeSet::from_iter(
-        (0..=28123).filter(|&num| utils::Divisors::new(num).sum::<i64>() > num + num),
-    );
+    let abundant_numbers = (0..=28123)
+        .filter(|&num| utils::Divisors::new(num).sum::<i64>() > num + num)
+        .collect::<Vec<i64>>();
     let sum: i64 = (0..=28123)
         .filter(|num| {
             // Can this number be expressed as the sum of two abundant numbers?
             for abundant1 in abundant_numbers
                 .iter()
-                .take_while(|abundant1| *abundant1 <= num)
+                .take_while(|&abundant1| abundant1 <= num)
             {
                 let abundant2 = num - abundant1;
-                if abundant_numbers.contains(&abundant2) {
-                    // It can be, so it must be filtered out.
-                    return false;
+                // If it can be, filter it out.
+                match abundant_numbers.binary_search(&abundant2) {
+                    Ok(_) => return false,
+                    _ => (),
                 }
             }
             true
