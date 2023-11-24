31ffa21ec2bfb076eab0ed4e2575f37727841723
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-24 19:55:05 +0530

    Formatting and minor fixes.

--- src/solutions/non_abundant_sums.rs
+++ src/solutions/non_abundant_sums.rs
@@ -2,23 +2,25 @@ use crate::utils;
 
 pub fn solve() -> i64 {
     let primes = utils::Primes::new(28124).iter().collect::<Vec<i64>>();
-    let abundant_numbers = (12..28124).filter(|&num| {
-        let mut num = num;
-        let sum_of_divisors_lb = num + num;
-        let mut sum_of_divisors = 1;
-        for &prime in &primes {
-            if prime > num {
-                break;
+    let abundant_numbers = (12..28124)
+        .filter(|&num| {
+            let mut num = num;
+            let sum_of_divisors_lb = num + num;
+            let mut sum_of_divisors = 1;
+            for &prime in &primes {
+                if prime > num {
+                    break;
+                }
+                let mut power = 0;
+                while num % prime == 0 {
+                    num /= prime;
+                    power += 1;
+                }
+                sum_of_divisors *= (prime.pow(power + 1) - 1) / (prime - 1);
             }
-            let mut power = 0;
-            while num % prime == 0 {
-                num /= prime;
-                power += 1;
-            }
-            sum_of_divisors *= (prime.pow(power + 1) - 1) / (prime - 1);
-        }
-        sum_of_divisors > sum_of_divisors_lb
-    }).collect::<Vec<i64>>();
+            sum_of_divisors > sum_of_divisors_lb
+        })
+        .collect::<Vec<i64>>();
 
     let sum = (0..28124)
         .filter(|&num| {
@@ -30,7 +32,7 @@ pub fn solve() -> i64 {
                 let abundant2 = num - abundant1;
                 match abundant_numbers.binary_search(&abundant2) {
                     Ok(_) => return false,
-                    _ => {}
+                    _ => (),
                 };
             }
             true
