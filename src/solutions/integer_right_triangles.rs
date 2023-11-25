0a9632ad601de4f408f20b7f83179114e3e4ed28
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-25 19:11:47 +0530

    Does not work, but committing anyway.

--- src/solutions/integer_right_triangles.rs
+++ src/solutions/integer_right_triangles.rs
@@ -1,3 +1,13 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    for p in (12..=130).step_by(2) {
+        println!("{} {:?}", p, utils::PythagoreanTriplets::new(p).collect::<Vec<(i64,i64,i64)>>());
+    }
+    let result = (12..=1000)
+        .map(|p| (utils::PythagoreanTriplets::new(p).count(), p))
+        .max()
+        .unwrap()
+        .1;
+    result as i64
 }
