c4d525371d2ae67aa249ef876b43dbd775366823
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-25 10:11:21 +0530

    Massive optimisation in P23.
    For some reason not quite clear to me, the approach I though was optimal (binary-searching for the number in a sorted vector which, when added to a particular number, yields an abundant sum) turned out to not be optimal. (Perhaps because of repetition?) Instead, adding all possible pairs to find all abundant sums, and then complementing that set is much faster! The running time went down from 480 ms to 60 ms. Wow.

--- src/solutions/non_abundant_sums.rs
+++ src/solutions/non_abundant_sums.rs
@@ -1,27 +1,27 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let abundant_numbers = (0..=28123)
+    let abundant_numbers = (0..28124)
         .filter(|&num| utils::Divisors::new(num).sum::<i64>() > num + num)
         .collect::<Vec<i64>>();
-    let sum: i64 = (0..=28123)
-        .filter(|num| {
-            // Can this number be expressed as the sum of two abundant numbers?
-            for abundant1 in abundant_numbers
-                .iter()
-                .take_while(|&abundant1| abundant1 <= num)
-            {
-                let abundant2 = num - abundant1;
-                // If it can be, filter it out.
-                match abundant_numbers.binary_search(&abundant2) {
-                    Ok(_) => return false,
-                    _ => (),
-                }
+
+    let mut abundant_sum = [false; 28124];
+    for i in 0..abundant_numbers.len() - 1 {
+        for j in i..abundant_numbers.len() {
+            let sum = (abundant_numbers[i] + abundant_numbers[j]) as usize;
+            if sum >= abundant_sum.len() {
+                break;
             }
-            true
-        })
+            abundant_sum[sum] = true;
+        }
+    }
+    let sum: usize = abundant_sum
+        .iter()
+        .enumerate()
+        .filter(|(_, &abundant)| !abundant)
+        .map(|(elem, _)| elem)
         .sum();
 
     assert_eq!(sum, 4179871);
-    sum
+    sum as i64
 }
