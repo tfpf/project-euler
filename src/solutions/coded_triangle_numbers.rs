d30e23dca5af0d22bd6eb837c4d143ddc84c6263
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-26 11:57:02 +0530

    Binary search is faster than hash search.

--- src/solutions/coded_triangle_numbers.rs
+++ src/solutions/coded_triangle_numbers.rs
@@ -2,8 +2,7 @@ use crate::utils;
 
 pub fn solve() -> i64 {
     // Collect enough triangle numbers to dwarf the greatest word value.
-    let triangle_numbers =
-        std::collections::HashSet::<i64>::from_iter(utils::Triangular::new().take(20));
+    let triangle_numbers = utils::Triangular::new().take(20).collect::<Vec<i64>>();
 
     let words = std::fs::read_to_string("res/coded_triangle_numbers.txt").unwrap();
     let result = words
@@ -11,7 +10,10 @@ pub fn solve() -> i64 {
         .map(|s| &s[1..s.len() - 1])
         .filter(|word| {
             let value = word.bytes().map(|b| b as i64 - 64).sum::<i64>();
-            triangle_numbers.contains(&value)
+            match triangle_numbers.binary_search(&value) {
+                Ok(_) => true,
+                _ => false
+            }
         })
         .count();
 
