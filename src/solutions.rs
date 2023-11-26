c52a7ae84adae7c8792cb63873877912622418d8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-26 11:28:52 +0530

    Formatted.

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,11 +1,11 @@
 pub mod amicable_numbers;
 pub mod champernownes_constant;
 pub mod circular_primes;
+pub mod coded_triangle_numbers;
 pub mod coin_sums;
 pub mod counting_sundays;
 pub mod digit_cancelling_fractions;
 pub mod digit_factorials;
-pub mod coded_triangle_numbers;
 pub mod digit_fifth_powers;
 pub mod distinct_powers;
 pub mod double_base_palindromes;
