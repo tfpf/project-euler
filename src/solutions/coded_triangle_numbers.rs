8b64083d0977fac50c652392f881630b1a622e7b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-26 11:49:26 +0530

    Optimised: no need to store the words in RAM.

--- src/solutions/coded_triangle_numbers.rs
+++ src/solutions/coded_triangle_numbers.rs
@@ -6,9 +6,9 @@ pub fn solve() -> i64 {
         std::collections::HashSet::<i64>::from_iter(utils::Triangular::new().take(20));
 
     let words = std::fs::read_to_string("res/coded_triangle_numbers.txt").unwrap();
-    let words: Vec<&str> = words.split(",").map(|s| &s[1..s.len() - 1]).collect();
     let result = words
-        .iter()
+        .split(",")
+        .map(|s| &s[1..s.len() - 1])
         .filter(|word| {
             let value = word.bytes().map(|b| b as i64 - 64).sum::<i64>();
             triangle_numbers.contains(&value)
