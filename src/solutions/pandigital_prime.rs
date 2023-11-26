42120f833e48d2515b9f3755871586b49c726463
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-26 10:56:39 +0530

    Reverse search speeds it up.

--- src/solutions/pandigital_prime.rs
+++ src/solutions/pandigital_prime.rs
@@ -5,10 +5,15 @@ pub fn solve() -> i64 {
     // with a different number of digits is divisible by 3. We get lucky while
     // searching 7-digit numbers.
     let mut pandigital_checker = utils::PandigitalChecker::new(7);
-    let result = (1234567..=7654321).step_by(2).filter(|&num| {
-        pandigital_checker.renew();
-        pandigital_checker.update(num) && pandigital_checker.check() && utils::is_prime(num)
-    }).last().unwrap();
+    let result = (1234567..=7654321)
+        .rev()
+        .step_by(2)
+        .filter(|&num| {
+            pandigital_checker.renew();
+            pandigital_checker.update(num) && pandigital_checker.check() && utils::is_prime(num)
+        })
+        .next()
+        .unwrap();
 
     assert_eq!(result, 7652413);
     result
