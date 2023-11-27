a24bd42c85baa47d746ed997973fbe126fc8765b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-27 20:06:37 +0530

    Made code more concise.

--- src/solutions/sub_string_divisibility.rs
+++ src/solutions/sub_string_divisibility.rs
@@ -11,30 +11,21 @@ pub fn solve() -> i64 {
         // the way first.
         if digits[3] & 1 != 0
             || (digits[2] + digits[3] + digits[4]) % 3 != 0
-            || digits[5] % 5 != 0
+            || (digits[5] != 0 && digits[5] != 5)
             || (digits[5] - digits[6] + digits[7]) % 11 != 0
         {
             continue;
         }
-        let check7 = digits[4..=6]
-            .iter()
-            .fold(0, |value, digit| value * 10 + digit);
-        if check7 % 7 != 0 {
+        if digits[4..=6].iter().fold(0, |value, d| value * 10 + d) % 7 != 0 {
             continue;
         }
-        let check13 = digits[6..=8]
-            .iter()
-            .fold(0, |value, digit| value * 10 + digit);
-        if check13 % 13 != 0 {
+        if digits[6..=8].iter().fold(0, |value, d| value * 10 + d) % 13 != 0 {
             continue;
         }
-        let check17 = digits[7..=9]
-            .iter()
-            .fold(0, |value, digit| value * 10 + digit);
-        if check17 % 17 != 0 {
+        if digits[7..=9].iter().fold(0, |value, d| value * 10 + d) % 17 != 0 {
             continue;
         }
-        sum += digits.iter().fold(0, |value, digit| value * 10 + digit);
+        sum += digits.iter().fold(0, |value, d| value * 10 + d);
     }
 
     assert_eq!(sum, 16695334890);
