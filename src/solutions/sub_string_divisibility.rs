85b5bdf7a6142d26d2ce93a7ff9099a8fe5c2f58
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-11-28 19:50:07 +0530

    Renamed variable.

--- src/solutions/sub_string_divisibility.rs
+++ src/solutions/sub_string_divisibility.rs
@@ -27,26 +27,28 @@ fn sub_string_divisible_sum(ds: &mut DigitsSet, idx: usize, value: i64) -> i64 {
     }
 
     // Try to place each available digit at the current index in turn.
-    let len = ds.len;
-    (0..len)
+    (0..ds.len)
         .map(|i| {
             let digit = ds.digits[i];
             let value = value * 10 + digit;
-            let bot_3 = value % 1000;
+            let subvalue = value % 1000;
 
             // Allow any digit to be placed at the first three indices. At any
-            // other index, allow a particular digit only if the number formed by
-            // taking the three least significant digits of the partially-built
-            // number is divisible by the appropriate prime number.
+            // other index, allow a particular digit only if the number formed
+            // by taking the three least significant digits of the
+            // partially-built number is divisible by the appropriate prime
+            // number.
             if idx <= 2
-                || (idx == 3 && bot_3 % 2 == 0)
-                || (idx == 4 && bot_3 % 3 == 0)
-                || (idx == 5 && bot_3 % 5 == 0)
-                || (idx == 6 && bot_3 % 7 == 0)
-                || (idx == 7 && bot_3 % 11 == 0)
-                || (idx == 8 && bot_3 % 13 == 0)
-                || (idx == 9 && bot_3 % 17 == 0)
+                || (idx == 3 && subvalue % 2 == 0)
+                || (idx == 4 && subvalue % 3 == 0)
+                || (idx == 5 && subvalue % 5 == 0)
+                || (idx == 6 && subvalue % 7 == 0)
+                || (idx == 7 && subvalue % 11 == 0)
+                || (idx == 8 && subvalue % 13 == 0)
+                || (idx == 9 && subvalue % 17 == 0)
             {
+                // Changing this number here does not affect the range iterator
+                // we are inside, because it received a copy.
                 ds.len -= 1;
                 (ds.digits[i], ds.digits[ds.len]) = (ds.digits[ds.len], ds.digits[i]);
                 let sum = sub_string_divisible_sum(ds, idx + 1, value);
