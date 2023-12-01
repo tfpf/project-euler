34658321ad5372526d5b7448cbb3768b89e5b81d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-01 23:28:07 +0530

    Removed triangular check.

--- src/solutions/triangular_pentagonal_and_hexagonal.rs
+++ src/solutions/triangular_pentagonal_and_hexagonal.rs
@@ -4,10 +4,13 @@ pub fn solve() -> i64 {
     let result = utils::Polygonal::new(6)
         .skip(143)
         .filter(|&num| {
-            utils::Polygonal::invert(3, num) != None && utils::Polygonal::invert(5, num) != None
+            // Every triangular number is a hexagonal number as well, so it is
+            // enough to check whether the number is pentagonal.
+            utils::Polygonal::invert(5, num) != None
         })
-        .next().unwrap();
+        .next()
+        .unwrap();
 
-    result
     assert_eq!(result, 1533776805);
+    result
 }
