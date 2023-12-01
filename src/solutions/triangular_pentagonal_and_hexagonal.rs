822305e0c03fcc04bb6fca8c290293e8cf892e65
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-01 23:23:50 +0530

    Solved P45 using `Polygonal::invert`.

--- src/solutions/triangular_pentagonal_and_hexagonal.rs
+++ src/solutions/triangular_pentagonal_and_hexagonal.rs
@@ -1,3 +1,13 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let result = utils::Polygonal::new(6)
+        .skip(143)
+        .filter(|&num| {
+            utils::Polygonal::invert(3, num) != None && utils::Polygonal::invert(5, num) != None
+        })
+        .next().unwrap();
+
+    result
+    assert_eq!(result, 1533776805);
 }
