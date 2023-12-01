56f0d918bbd6a3d4b7d7feb832f9cf12a993764e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 00:41:37 +0530

    Brute-forced the solution.

--- src/solutions/goldbachs_other_conjecture.rs
+++ src/solutions/goldbachs_other_conjecture.rs
@@ -1,3 +1,24 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let mut arr = vec![false; 10000];
+    let primes = utils::Primes::new(10000);
+    for prime in primes.iter() {
+        for square in utils::Polygonal::new(4) {
+            let num = prime + 2 * square;
+            if (num as usize) < arr.len() {
+                arr[num as usize] = true;
+            } else {
+                break;
+            }
+        }
+    }
+    let result = (3..arr.len())
+        .step_by(2)
+        .filter(|&num| !utils::is_prime(num as i64) && !arr[num])
+        .next()
+        .unwrap();
+
+    assert_eq!(result, 5777);
+    result as i64
 }
