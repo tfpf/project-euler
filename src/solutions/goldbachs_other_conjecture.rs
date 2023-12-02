eab1761f66bf14279e437e866a60d014e07c80d9
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 12:24:20 +0530

    Problem 46 (#48)

---
+++ src/solutions/goldbachs_other_conjecture.rs
@@ -0,0 +1,34 @@
+use crate::utils;
+
+pub fn solve() -> i64 {
+    const LIMIT: usize = 10000;
+
+    // This indicates which numbers can be constructed in the described manner.
+    // Since only odd numbers are of interest, half of the space used is
+    // wasted. Doing extra processing to prevent wastage increases the running
+    // time by more than 100 µs.
+    let mut goldbach_constructible = vec![false; LIMIT];
+
+    let primes = utils::Primes::new(LIMIT);
+    let sieve = primes.sieve();
+    for prime in primes.iter() {
+        // Squares are quadrilateral numbers. Generating them like this avoids
+        // having to do multiplication.
+        for square in utils::Polygonal::new(4) {
+            let num = (prime + 2 * square) as usize;
+            if num < goldbach_constructible.len() {
+                goldbach_constructible[num] = true;
+            } else {
+                break;
+            }
+        }
+    }
+    let result = (3..goldbach_constructible.len())
+        .step_by(2)
+        .filter(|&idx| !goldbach_constructible[idx] && !sieve[idx])
+        .next()
+        .unwrap();
+
+    assert_eq!(result, 5777);
+    result as i64
+}
