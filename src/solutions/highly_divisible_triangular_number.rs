b54a5f0162417a63bb87d17cfbe51335423e0b54
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 18:33:31 +0530

    Test commit demonstrating what previous commit message said.

--- src/solutions/highly_divisible_triangular_number.rs
+++ src/solutions/highly_divisible_triangular_number.rs
@@ -1,11 +1,23 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
+    let primes = utils::Primes::new(76576501).iter().collect::<Vec<i64>>();
     let result = utils::Polygonal::new(3)
         .filter(|&num| {
-            utils::PrimeDivisors::new(num)
-                .fold(1, |accumulator, (_, power)| accumulator * (power + 1))
-                >= 500
+            let mut num = num;
+            let mut divisors = 1;
+            for &prime in primes.iter() {
+                if num < prime {
+                    break;
+                }
+                let mut power = 0;
+                while num % prime == 0 {
+                    num /= prime;
+                    power += 1;
+                }
+                divisors *= 1 + power;
+            }
+            divisors >= 500
         })
         .next()
         .unwrap();
