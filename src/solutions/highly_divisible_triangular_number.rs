262e3a5ec417037ae97f5ff1c6465ef83010ad55
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 19:20:12 +0530

    P12 optimisation (#51)
    Found this trick in the thread for P12. Reduces running time from 100 ms to 20 ms. Another optimisation removes an extra calculation: divisors of one factor are already calculated in the previous loop iteration. Using it instead of recalculating reduces the time to 10 ms.
    
    Counting the divisors using `Divisors` is faster than using `PrimeDivisors`, but I will keep it because it may be useful for some other problem later on.

--- src/solutions/highly_divisible_triangular_number.rs
+++ src/solutions/highly_divisible_triangular_number.rs
@@ -1,14 +1,22 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let result = utils::Polygonal::new(3)
-        .filter(|&num| {
-            utils::PrimeDivisors::new(num)
-                .fold(1, |accumulator, (_, power)| accumulator * (power + 1))
-                >= 500
+    // The number of divisors of the product of two coprime numbers is the
+    // product of their number of divisors. Every triangular number is the
+    // product of two coprime numbers. Just iterate over the indices in order
+    // to have those two coprime numbers at hand.
+    let mut multiplicand1_divisors = 1;
+    let idx = (1..)
+        .filter(|&idx| {
+            let multiplicand2 = if idx & 1 == 1 { (idx + 1) / 2 } else { idx + 1 };
+            let multiplicand2_divisors = utils::Divisors::new(multiplicand2).count();
+            let divisors = multiplicand1_divisors * multiplicand2_divisors;
+            multiplicand1_divisors = multiplicand2_divisors;
+            divisors >= 500
         })
         .next()
         .unwrap();
+    let result = idx * (idx + 1) / 2;
 
     assert_eq!(result, 76576500);
     result
