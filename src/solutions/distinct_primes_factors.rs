49de1a10816fff86f74f5bd3ac4eb4629ea5c5d6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 19:35:42 +0530

    Added non-optimal solution.

--- src/solutions/distinct_primes_factors.rs
+++ src/solutions/distinct_primes_factors.rs
@@ -1,3 +1,23 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let result = (644..)
+        .filter(|&num| {
+            for n in num..num + 4 {
+                if std::collections::HashSet::<i64>::from_iter(
+                    utils::PrimeDivisors::new(n).map(|(prime, _)| prime),
+                )
+                .len()
+                    < 4
+                {
+                    return false;
+                }
+            }
+            true
+        })
+        .next()
+        .unwrap();
+
+    assert_eq!(result, 134043)
+    result
 }
