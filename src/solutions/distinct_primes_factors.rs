54b3124fa37fa7b8c6d74c8176f2a875557920e7
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-02 23:41:59 +0530

    More optimisations inspired by Boyer and Moore.
    Reducing the time from 7000 ms to 1800 ms.

--- src/solutions/distinct_primes_factors.rs
+++ src/solutions/distinct_primes_factors.rs
@@ -1,17 +1,33 @@
 use crate::utils;
 
-pub fn solve() -> i64 {
-    let result = (644..)
-        .filter(|&num| {
-            for n in num..num + 4 {
-                if utils::PrimeDivisors::new(n).count() < 4 {
-                    return false;
+fn four_distinct() -> i64 {
+    let mut num = 644;
+    let mut obtained = 0;
+    let mut required = 4;
+    loop {
+        // Search backwards (similar to how it's done in the Boyer-Moore
+        // substring searching algorithm) so that we can skip forwards,
+        // avoiding unnecessary computations.
+        for n in (num..num + 4).rev() {
+            // Try to terminate the iterator as early as possible by taking
+            // only 4 prime divisors.
+            if utils::PrimeDivisors::new(n).take(4).count() == 4 {
+                required -= 1;
+                if required == 0 {
+                    return num;
                 }
+            } else {
+                num += obtained + required;
+                obtained = 4 - obtained - required;
+                required = 4 - obtained;
+                break;
             }
-            true
-        })
-        .next()
-        .unwrap();
+        }
+    }
+}
+
+pub fn solve() -> i64 {
+    let result = four_distinct();
 
     assert_eq!(result, 134043);
     result
