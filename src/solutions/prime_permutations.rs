87538739d705f4d975c1d098311cb603ebe211dc
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-03 18:58:08 +0530

    Quick-and-dirty solution.

--- src/solutions/prime_permutations.rs
+++ src/solutions/prime_permutations.rs
@@ -1,3 +1,50 @@
+use crate::utils;
+
+/// Find the frequencies of all digits in the given number.
+///
+/// * `num` - Number to analyse.
+///
+/// -> Map between digits and frequencies.
+fn digits_frequencies(num: i64) -> [i32; 10] {
+    let mut freq = [0; 10];
+    for digit in utils::Digits::new(num) {
+        freq[digit as usize] += 1;
+    }
+    freq
+}
+
+/// Find three prime numbers which are in arithmetic progression and consist of
+/// the same digits.
+///
+/// -> Tuple of prime numbers.
+fn prime_permutations() -> (i64, i64, i64) {
+    let primes = utils::Primes::new(9999);
+    let sieve = primes.sieve();
+    let primes = primes
+        .iter()
+        .skip_while(|&prime| prime < 1000)
+        .collect::<Vec<i64>>();
+    for i in 1..primes.len() {
+        let freqi = digits_frequencies(primes[i]);
+        for j in 0..i {
+            let candidate = 2 * primes[j] - primes[i];
+            if candidate >= 1000
+                && candidate != 1487
+                && sieve[candidate as usize]
+                && digits_frequencies(primes[j]) == freqi
+                && digits_frequencies(candidate) == freqi
+            {
+                return (candidate, primes[j], primes[i]);
+            }
+        }
+    }
+    unreachable!();
+}
+
 pub fn solve() -> i64 {
-    0
+    let result = prime_permutations();
+    let result = result.0 * 100000000 + result.1 * 10000 + result.2;
+
+    assert_eq!(result, 296962999629);
+    result
 }
