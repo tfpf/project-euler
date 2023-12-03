f8528c1add4a83bdbf2f3029eb87307a242a0739
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-03 10:04:59 +0530

    P42 optimisation using `Polygonal::invert`.
    As noted in b1de133168700d2863694a35a113effd1e9e1b79, using the quadratic formula to check whether a number is triangular (170 µs) is faster than binary-searching for it in a list of triangular numbers (240 µs).

--- src/solutions/coded_triangle_numbers.rs
+++ src/solutions/coded_triangle_numbers.rs
@@ -1,19 +1,13 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    // Collect enough triangle numbers to dwarf the greatest word value.
-    let triangle_numbers = utils::Polygonal::new(3).take(20).collect::<Vec<i64>>();
-
     let words = std::fs::read_to_string("res/coded_triangle_numbers.txt").unwrap();
     let result = words
         .split(",")
         .map(|s| &s[1..s.len() - 1])
         .filter(|word| {
             let value = word.bytes().map(|b| b as i64 - 64).sum::<i64>();
-            match triangle_numbers.binary_search(&value) {
-                Ok(_) => true,
-                Err(_) => false,
-            }
+            utils::Polygonal::invert(3, value) != None
         })
         .count();
 
