d879dd390a0d06beebe8e873e2966549f68b5362
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-03 19:14:45 +0530

    Optimised: comparing digits is enough.
    No need to compare digits frequency table.

--- src/solutions/prime_permutations.rs
+++ src/solutions/prime_permutations.rs
@@ -1,16 +1,14 @@
 use crate::utils;
 
-/// Find the frequencies of all digits in the given number.
+/// Find the digits of a number in sorted order.
 ///
 /// * `num` - Number to analyse.
 ///
-/// -> Map between digits and frequencies.
-fn digits_frequencies(num: i64) -> [i32; 10] {
-    let mut freq = [0; 10];
-    for digit in utils::Digits::new(num) {
-        freq[digit as usize] += 1;
-    }
-    freq
+/// -> Digits in sorted order.
+fn digits(num: i64) -> Vec<i64> {
+    let mut digits = utils::Digits::new(num).collect::<Vec<i64>>();
+    digits.sort();
+    digits
 }
 
 /// Find three prime numbers which are in arithmetic progression and consist of
@@ -28,14 +26,17 @@ fn prime_permutations() -> (i64, i64, i64) {
     // The outer index will find the largest required number. The inner index
     // will find the middle number. Hence, it makes sense to go in reverse.
     for i in (1..primes.len()).rev() {
-        let freqi = digits_frequencies(primes[i]);
+        if primes[i] == 8147 {
+            continue;
+        }
+
+        let di = digits(primes[i]);
         for j in (0..i).rev() {
             let candidate = 2 * primes[j] - primes[i];
             if candidate >= 1000
-                && candidate != 1487
                 && sieve[candidate as usize]
-                && digits_frequencies(primes[j]) == freqi
-                && digits_frequencies(candidate) == freqi
+                && digits(primes[j]) == di
+                && digits(candidate) == di
             {
                 return (candidate, primes[j], primes[i]);
             }
