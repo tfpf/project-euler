f7e723bbf75bba29efb5959b3860d8298ecbdb3a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-04 19:13:08 +0530

    Working, but show.
    Does not print the solution, but finds it all right in 7000 ms.

--- src/solutions/consecutive_prime_sum.rs
+++ src/solutions/consecutive_prime_sum.rs
@@ -1,3 +1,38 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
+    let primes = utils::Primes::new(1000000).iter().skip_while(|&prime| prime < 1000).collect::<Vec<i64>>();
+
+    for i in (2..primes.len()).rev() {
+        let mut window = (i - 2, i - 1);
+        let mut sum = primes[window.0] + primes[window.1];
+        while window.0 >= 1 {
+            if sum == primes[i] {
+                window.0 -= 1;
+                sum += primes[window.0] - primes[window.1];
+                window.1 -= 1;
+            }
+            else if sum > primes[i] {
+                sum -= primes[window.1];
+                window.1 -= 1;
+            }
+            else {
+                window.0 -= 1;
+                sum += primes[window.0];
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
     0
 }
