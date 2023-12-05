e6f212effcc07d40be5b43fe4eacefa79fc0b0d7
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-05 10:08:22 +0530

    Committing so I can switch branches.

--- src/solutions/prime_digit_replacements.rs
+++ src/solutions/prime_digit_replacements.rs
@@ -1,3 +1,40 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
+    let primes = utils::Primes::new(100000);
+    let sieve = primes.sieve();
+    let primes = primes.iter().collect::<Vec<i64>>();
+    for i in 1..primes.len() {
+        for j in 0..i {
+            let mut difference = primes[i] - primes[j];
+            let mut tenpower = 1;
+            while difference % 10 == 0 {
+                difference /= 10;
+                tenpower *= 10;
+            }
+            let digit = difference % 10;
+            if difference % digit == 0 {
+                difference /= digit;
+            }
+            match difference {
+                11 | 101 | 1001 | 10001 | 100001 => (),
+                _ => continue,
+            }
+            let difference = difference * tenpower;
+            if (0..=9).map(|digit| (primes[i] + difference * digit) as usize).filter(|&num| num < sieve.len()).map(|num| sieve[num]).filter(|&p| p).count() >= 4 {
+                println!("{} {} {}", primes[i], primes[i] + difference, primes[i] + difference * 2);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
     0
 }
