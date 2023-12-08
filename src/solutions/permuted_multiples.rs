a6f7172d1be442ed33b8f09cdf6d28089c4a9efd
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-08 22:56:21 +0530

    Problem 52. (#62)
    * Moved `digits_frequencies` to `utils`.
    * Try to solve all problems from 1 to 100.
      * Since I have broken the order in which I am solving the problems, this is necessary, so that the tests on GitHub Actions run correctly.

---
+++ src/solutions/permuted_multiples.rs
@@ -0,0 +1,27 @@
+use crate::utils;
+
+pub fn solve() -> i64 {
+    // The number must have at least three digits (else, there are fewer than
+    // six permutations possible). Its most significant digit must be 1 (else,
+    // multiplying it by 6 would increase the number of digits). (In fact, the
+    // upper bound is the number starting with 1 and containing 6s, for the
+    // same reason.) Actually, someone who has played around with a calculator
+    // in their youth might know the answer by heart!
+    let result = (100..166)
+        .chain(1000..1666)
+        .chain(10000..16666)
+        .chain(100000..166666)
+        .filter(|&num| {
+            let expected = utils::digits_frequencies(num);
+            expected == utils::digits_frequencies(2 * num)
+                && expected == utils::digits_frequencies(3 * num)
+                && expected == utils::digits_frequencies(4 * num)
+                && expected == utils::digits_frequencies(5 * num)
+                && expected == utils::digits_frequencies(6 * num)
+        })
+        .next()
+        .unwrap();
+
+    assert_eq!(result, 142857);
+    result
+}
