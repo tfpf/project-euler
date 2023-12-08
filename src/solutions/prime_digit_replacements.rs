90b3c105517658a0e3e408d20e4416d1c0161d05
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-08 22:28:44 +0530

    This does not work, either.

--- src/solutions/prime_digit_replacements.rs
+++ src/solutions/prime_digit_replacements.rs
@@ -1,40 +1,32 @@
 use crate::utils;
 
-pub fn solve() -> i64 {
-    let primes = utils::Primes::new(100000);
-    let sieve = primes.sieve();
-    let primes = primes.iter().collect::<Vec<i64>>();
-    for i in 1..primes.len() {
-        for j in 0..i {
-            let mut difference = primes[i] - primes[j];
-            let mut tenpower = 1;
-            while difference % 10 == 0 {
-                difference /= 10;
-                tenpower *= 10;
-            }
-            let digit = difference % 10;
-            if difference % digit == 0 {
-                difference /= digit;
-            }
-            match difference {
-                11 | 101 | 1001 | 10001 | 100001 => (),
-                _ => continue,
-            }
-            let difference = difference * tenpower;
-            if (0..=9).map(|digit| (primes[i] + difference * digit) as usize).filter(|&num| num < sieve.len()).map(|num| sieve[num]).filter(|&p| p).count() >= 4 {
-                println!("{} {} {}", primes[i], primes[i] + difference, primes[i] + difference * 2);
-            }
+fn replacements_are_prime(prime: i64, sieve: &utils::SieveOfEratosthenes) -> bool {
+    for difference in [11, 101, 110, 1001, 1010, 1100, 10001, 10010, 10100, 11000, 100001, 100010, 100100, 101000, 110000, 1000001, 1000010, 1000100, 1001000, 1010000, 1100000] {
+        if difference > prime {
+            break;
+        }
+        if (0..=9).filter(|digit| {
+            let num = prime + digit * difference;
+            utils::is_prime(num)
+        }).count() >= 8 {
+            return true;
         }
     }
 
 
 
 
+    false
+}
 
-
-
-
-
-
+pub fn solve() -> i64 {
+    let sieve = utils::SieveOfEratosthenes::new(100000000);
+    for prime in sieve.iter() {
+        if replacements_are_prime(prime, &sieve) {
+            println!("{}", prime);
+        }
+    }
+    // After finding a prime number, check whether the numbers obtained by
+    // adding 11, 22, 33 and so on (or 101, 202, 303 and so on) are prime.
     0
 }
