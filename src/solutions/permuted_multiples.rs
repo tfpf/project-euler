19ee23b14d37d2b551ae9f6b14c427eb41d04856
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-08 22:47:01 +0530

    Removed impossible numbers from search.

--- src/solutions/permuted_multiples.rs
+++ src/solutions/permuted_multiples.rs
@@ -1,7 +1,12 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let result = (1..)
+    // The number must have at least three digits (else, there are fewer than
+    // six permutations possible). Its most significant digit must be 1 (else,
+    // multiplying it by 6 would increase the number of digits). Actually,
+    // someone who has played around with a calculator in their youth might
+    // know the answer by heart!
+    let result = (100..200).chain(1000..2000).chain(10000..20000).chain(100000..200000)
         .filter(|&num| {
             let expected = utils::digits_frequencies(num);
             expected == utils::digits_frequencies(2 * num)
