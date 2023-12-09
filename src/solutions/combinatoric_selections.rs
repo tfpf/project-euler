2bd8ed000d42c33dc35fa9ac27f8edb4eb301599
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-09 10:02:42 +0530

    Consolidated initialisations.

--- src/solutions/combinatoric_selections.rs
+++ src/solutions/combinatoric_selections.rs
@@ -4,11 +4,8 @@ pub fn solve() -> i64 {
     // Determine all binomial coefficients by constructing Pascal's triangle.
     // As an optimisation, store only one row, overwriting it to generate the
     // next row.
-    let mut triangle = [0; 101];
-    triangle[0] = 1;
+    let mut triangle = [1; 101];
     for row in 1..=100 {
-        triangle[0] = 1;
-        triangle[row] = 1;
         let above_1_mil = (1..row).rev().fold(0, |above_1_mil, col| {
             triangle[col] += triangle[col - 1];
             if triangle[col] > 1000000 {
