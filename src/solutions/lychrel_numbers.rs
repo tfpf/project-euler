9a9ca04318428016b81a2aaec15ed3e3e63d1ddf
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-10 00:37:41 +0530

    Eliminated a mutable variable.

--- src/solutions/lychrel_numbers.rs
+++ src/solutions/lychrel_numbers.rs
@@ -4,13 +4,13 @@ pub fn solve() -> i64 {
     let result = (0..10000)
         .filter(|&num| {
             let mut num = utils::Long::from(num);
-            let mut num_rev = num.reverse();
-            for _ in 0..50 {
-                num += &num_rev;
-                num_rev = num.reverse();
+            num += &num.reverse();
+            for _ in 1..50 {
+                let num_rev = num.reverse();
                 if num == num_rev {
                     return false;
                 }
+                num += &num_rev;
             }
             true
         })
