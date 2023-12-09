0784f9fc31d7048ded1e9dbc6d63da464c327b88
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-09 14:44:40 +0530

    Moved `PokerHand` to `utils`.

--- src/solutions/poker_hands.rs
+++ src/solutions/poker_hands.rs
@@ -1,124 +1,5 @@
+use crate::utils;
 use std::io::BufRead;
-
-#[derive(Debug)]
-struct PokerHand {
-    // Pairs in which the first number is the value and the second is the suit.
-    hand: Vec<(u8, u8)>,
-    score: u8,
-}
-impl PokerHand {
-    const ROYAL_FLUSH: u8 = 0;
-    const STRAIGHT_FLUSH: u8 = 1;
-    const FOUR_OF_A_KIND: u8 = 2;
-    const FULL_HOUSE: u8 = 3;
-    const FLUSH: u8 = 4;
-    const STRAIGHT: u8 = 5;
-    const THREE_OF_A_KIND: u8 = 6;
-    const TWO_PAIRS: u8 = 7;
-    const ONE_PAIR: u8 = 8;
-    const HIGH_CARD: u8 = 9;
-}
-impl PokerHand {
-    fn new(hand: &[&str]) -> PokerHand {
-        let hand = hand
-            .iter()
-            .map(|card| {
-                let card = card.as_bytes();
-                let value = match card[0] {
-                    b'T' => 10,
-                    b'J' => 11,
-                    b'Q' => 12,
-                    b'K' => 13,
-                    b'A' => 14,
-                    value => value - b'0',
-                };
-                let suit = card[1];
-                (value, suit)
-            })
-            .collect();
-        let mut poker_hand = PokerHand {
-            hand: hand,
-            score: 0,
-        };
-        poker_hand.calculate_score();
-        poker_hand
-    }
-    fn calculate_score(&mut self) {
-        self.hand.sort();
-        let same_suit = self
-            .hand
-            .iter()
-            .skip(1)
-            .all(|card| card.1 == self.hand[0].1);
-        let consecutive_values = self
-            .hand
-            .iter()
-            .enumerate()
-            .skip(1)
-            .all(|(idx, card)| card.0 == self.hand[idx - 1].0 + 1);
-
-        if same_suit && consecutive_values {
-            if self.hand[0].0 == 10 {
-                self.score = PokerHand::ROYAL_FLUSH;
-            } else {
-                self.score = PokerHand::STRAIGHT_FLUSH;
-            }
-            return;
-        }
-
-        let mut values_frequencies = [0; 15];
-        let (twos, threes, fours) =
-            self.hand
-                .iter()
-                .fold((0, 0, 0), |(twos, threes, fours), card| {
-                    let value = card.0 as usize;
-                    values_frequencies[value] += 1;
-                    match values_frequencies[value] {
-                        2 => (twos + 1, threes, fours),
-                        3 => (twos - 1, threes + 1, fours),
-                        4 => (twos, threes - 1, fours + 1),
-                        _ => (twos, threes, fours),
-                    }
-                });
-
-        if fours == 1 {
-            self.score = PokerHand::FOUR_OF_A_KIND;
-            return;
-        }
-        if twos == 1 && threes == 1 {
-            self.score = PokerHand::FULL_HOUSE;
-            return;
-        }
-        if same_suit {
-            self.score = PokerHand::FLUSH;
-            return;
-        }
-        if consecutive_values {
-            self.score = PokerHand::STRAIGHT;
-            return;
-        }
-        if threes == 1 {
-            self.score = PokerHand::THREE_OF_A_KIND;
-            return;
-        }
-        if twos == 2 {
-            self.score = PokerHand::TWO_PAIRS;
-            return;
-        }
-        if twos == 1 {
-            self.score = PokerHand::ONE_PAIR;
-            return;
-        }
-        self.score = PokerHand::HIGH_CARD;
-    }
-}
-
-fn find_winner(p1_hand: &[&str], p2_hand: &[&str]) -> bool {
-    let p1_hand = PokerHand::new(p1_hand);
-    let p2_hand = PokerHand::new(p2_hand);
-    println!("{:?}", p1_hand);
-    false
-}
 
 pub fn solve() -> i64 {
     let fhandle = std::fs::File::open("res/poker_hands.txt").unwrap();
@@ -127,7 +8,10 @@ pub fn solve() -> i64 {
         .lines()
         .filter(|line| {
             let split = line.as_ref().unwrap().split(" ").collect::<Vec<&str>>();
-            find_winner(&split[..5], &split[5..])
+            let p1_hand = utils::PokerHand::new(&split[..5]);
+            let p2_hand = utils::PokerHand::new(&split[5..]);
+            println!("{:?} {:?}", p1_hand, p2_hand);
+            p1_hand > p2_hand
         })
         .count();
 
--- src/utils.rs
+++ src/utils.rs
@@ -530,6 +530,144 @@ impl SieveOfEratosthenes {
     }
 }
 
+/// A hand of poker.
+#[derive(Eq, Debug, PartialEq)]
+pub struct PokerHand {
+    // Pairs in which the first number is the value of a card and the second is
+    // its suit. The pairs shall be sorted in descending order of the value.
+    hand: Vec<(u8, u8)>,
+    // Used to rank this hand on an arbitrary scale. Higher is better.
+    score: u8,
+}
+impl PokerHand {
+    const HIGH_CARD: u8 = 0;
+    const ONE_PAIR: u8 = 1;
+    const TWO_PAIRS: u8 = 2;
+    const THREE_OF_A_KIND: u8 = 3;
+    const STRAIGHT: u8 = 4;
+    const FLUSH: u8 = 5;
+    const FULL_HOUSE: u8 = 6;
+    const FOUR_OF_A_KIND: u8 = 7;
+    const STRAIGHT_FLUSH: u8 = 8;
+    const ROYAL_FLUSH: u8 = 9;
+}
+impl PokerHand {
+    pub fn new(hand: &[&str]) -> PokerHand {
+        let hand = hand
+            .iter()
+            .map(|card| {
+                let card = card.as_bytes();
+                let value = match card[0] {
+                    b'T' => 10,
+                    b'J' => 11,
+                    b'Q' => 12,
+                    b'K' => 13,
+                    b'A' => 14,
+                    value => value - b'0',
+                };
+                let suit = card[1];
+                (value, suit)
+            })
+            .collect();
+        let mut poker_hand = PokerHand {
+            hand: hand,
+            score: 0,
+        };
+        poker_hand.calculate_score();
+        poker_hand
+    }
+    fn calculate_score(&mut self) {
+        // Sorting the cards by descending order of value makes analysing them
+        // easier.
+        self.hand.sort_by(|a, b| b.cmp(a));
+        let same_suit = self
+            .hand
+            .iter()
+            .skip(1)
+            .all(|card| card.1 == self.hand[0].1);
+        let consecutive_values = self
+            .hand
+            .iter()
+            .enumerate()
+            .skip(1)
+            .all(|(idx, card)| card.0 == self.hand[idx - 1].0 - 1);
+
+        if same_suit && consecutive_values {
+            if self.hand[0].0 == 14 {
+                self.score = PokerHand::ROYAL_FLUSH;
+            } else {
+                self.score = PokerHand::STRAIGHT_FLUSH;
+            }
+            return;
+        }
+
+        let mut values_frequencies = [0; 15];
+        let (twos, threes, fours) =
+            self.hand
+                .iter()
+                .fold((0, 0, 0), |(twos, threes, fours), card| {
+                    let value = card.0 as usize;
+                    values_frequencies[value] += 1;
+                    match values_frequencies[value] {
+                        2 => (twos + 1, threes, fours),
+                        3 => (twos - 1, threes + 1, fours),
+                        4 => (twos, threes - 1, fours + 1),
+                        _ => (twos, threes, fours),
+                    }
+                });
+
+        if fours == 1 {
+            self.score = PokerHand::FOUR_OF_A_KIND;
+            return;
+        }
+        if twos == 1 && threes == 1 {
+            self.score = PokerHand::FULL_HOUSE;
+            return;
+        }
+        if same_suit {
+            self.score = PokerHand::FLUSH;
+            return;
+        }
+        if consecutive_values {
+            self.score = PokerHand::STRAIGHT;
+            return;
+        }
+        if threes == 1 {
+            self.score = PokerHand::THREE_OF_A_KIND;
+            return;
+        }
+        if twos == 2 {
+            self.score = PokerHand::TWO_PAIRS;
+            return;
+        }
+        if twos == 1 {
+            self.score = PokerHand::ONE_PAIR;
+            return;
+        }
+        self.score = PokerHand::HIGH_CARD;
+    }
+}
+impl Ord for PokerHand {
+    fn cmp(&self, other: &PokerHand) -> std::cmp::Ordering {
+        let compare = self.score.cmp(&other.score);
+        if compare != std::cmp::Ordering::Equal {
+            return compare;
+        }
+        for (scard, ocard) in self.hand.iter().zip(other.hand.iter()) {
+            let compare = scard.0.cmp(&ocard.0);
+            if compare != std::cmp::Ordering::Equal {
+                return compare;
+            }
+        }
+        std::cmp::Ordering::Equal
+    }
+}
+impl PartialOrd for PokerHand {
+    fn partial_cmp(&self, other: &PokerHand) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 /******************************************************************************
  * Iterators.
  *****************************************************************************/
