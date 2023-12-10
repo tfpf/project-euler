06e4aa51178642f4dd6f6ec3d51e924fc44b6eba
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-10 11:20:57 +0530

    Rustified. Similar performance.

--- src/solutions/powerful_digit_sum.rs
+++ src/solutions/powerful_digit_sum.rs
@@ -1,14 +1,19 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let mut result = 0;
-    for a in 1..100 {
-        let mut num = utils::Long::from(1);
-        for b in 1..100 {
-            num *= a;
-            result = std::cmp::max(result, num.to_string().bytes().map(|byte| (byte - b'0') as i32).sum());
-        }
-    }
+    let result: i32 = (1..100)
+        .flat_map(|a| {
+            let mut num = utils::Long::from(1);
+            (1..100).map(move |_| {
+                num *= a;
+                num.to_string()
+                    .bytes()
+                    .map(|byte| (byte - b'0') as i32)
+                    .sum()
+            })
+        })
+        .max()
+        .unwrap();
 
     assert_eq!(result, 972);
     result as i64
