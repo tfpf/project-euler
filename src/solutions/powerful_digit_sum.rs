776cf0f128bd97fdd770da822d0a1dd68c456a7e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-10 11:14:55 +0530

    Solved.

--- src/solutions/powerful_digit_sum.rs
+++ src/solutions/powerful_digit_sum.rs
@@ -1,3 +1,15 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let mut result = 0;
+    for a in 1..100 {
+        let mut num = utils::Long::from(1);
+        for b in 1..100 {
+            num *= a;
+            result = std::cmp::max(result, num.to_string().bytes().map(|byte| (byte - b'0') as i32).sum());
+        }
+    }
+
+    assert_eq!(result, 972);
+    result as i64
 }
