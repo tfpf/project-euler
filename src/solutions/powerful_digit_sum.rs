25be8759ff225d0041427f1feadbb3f5988b409e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-10 11:27:51 +0530

    Minor optimisation.

--- src/solutions/powerful_digit_sum.rs
+++ src/solutions/powerful_digit_sum.rs
@@ -3,8 +3,10 @@ use crate::utils;
 pub fn solve() -> i64 {
     let result: i32 = (1..100)
         .flat_map(|a| {
-            let mut num = utils::Long::from(1);
-            (1..100).map(move |_| {
+            // Start from raising numbers to 2, because, by simple observation,
+            // the maximum sum can never be obtained by raising numbers to 1.
+            let mut num = utils::Long::from(a);
+            (2..100).map(move |_| {
                 num *= a;
                 num.to_string()
                     .bytes()
