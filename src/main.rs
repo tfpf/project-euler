c766177d0aac7c7b8eec8651525425516e63e374
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-11 22:23:14 +0530

    Problem 57. (#73)

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,7 @@ fn solve_and_time_one(problem_number: i32) -> bool {
         54 => solutions::poker_hands::solve,
         55 => solutions::lychrel_numbers::solve,
         56 => solutions::powerful_digit_sum::solve,
+        57 => solutions::square_root_convergents::solve,
         67 => solutions::maximum_path_sum_ii::solve,
         _ => return false,
     };
