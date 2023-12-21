26280f9b3842315cb3d033605fd1a528a0f438aa
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-22 00:11:45 +0530

    Wrote something, but it is not working.

--- src/solutions/cubic_permutations.rs
+++ src/solutions/cubic_permutations.rs
@@ -1,3 +1,24 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
+    let mut frequency = std::collections::BTreeMap::new();
+    for cube in utils::Cubes::new() {
+        let mut digits = utils::Digits::new(cube).collect::<Vec<i64>>();
+        digits.sort();
+        let key = digits
+            .iter()
+            .rev()
+            .fold(0, |key, digit| key * 10 + digit);
+        match frequency.get_mut(&key) {
+            Some(count) => {
+                *count += 1;
+            }
+            None => {
+                frequency.insert(key, 1);
+            }
+        }
+        println!("{} {} {}", cube, key, frequency.get(&key).unwrap());
+    }
+
     0
 }
--- src/utils.rs
+++ src/utils.rs
@@ -929,6 +929,31 @@ impl Iterator for Polygonal {
     }
 }
 
+/// Cubes iterator.
+pub struct Cubes {
+    increment: i64,
+    offset: i64,
+    num: i64,
+}
+impl Cubes {
+    pub fn new() -> Cubes {
+        Cubes {
+            increment: 6,
+            offset: 1,
+            num: 0,
+        }
+    }
+}
+impl Iterator for Cubes {
+    type Item = i64;
+    fn next(&mut self) -> Option<i64> {
+        self.num += self.offset;
+        self.offset += self.increment;
+        self.increment += 6;
+        Some(self.num)
+    }
+}
+
 /// Collatz sequence iterator.
 pub struct Collatz {
     num: i64,
