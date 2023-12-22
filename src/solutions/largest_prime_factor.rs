15b85a514848d1cbef303f88f1fb38c6dcd7c462
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-22 22:20:05 +0530

    FIXME done; P3 RT reduced from 6.8 ms to 27 µs.
    How convenient that I had already implemented `PrimeDivisors`!. How lucky that I did not remove it! (As noted in 262e3a5ec417037ae97f5ff1c6465ef83010ad55, I kept it because I thought I might need it later.)

--- src/solutions/largest_prime_factor.rs
+++ src/solutions/largest_prime_factor.rs
@@ -2,24 +2,7 @@ use crate::utils;
 
 pub fn solve() -> i64 {
     let num: i64 = 600851475143;
-    let largest_pf = (3i64..)
-        .step_by(2)
-        .take_while(|f| f * f <= num)
-        // FIXME This doesn't seem correct. What if `f` is composite, but
-        // `other` is prime?
-        .filter_map(|f| {
-            if num % f != 0 || !utils::is_prime(f) {
-                return None;
-            }
-            let other = num / f;
-            if utils::is_prime(other) {
-                Some(other)
-            } else {
-                Some(f)
-            }
-        })
-        .max()
-        .unwrap();
+    let largest_pf = utils::PrimeDivisors::new(num).max().unwrap().0;
 
     assert_eq!(largest_pf, 6857);
     largest_pf
