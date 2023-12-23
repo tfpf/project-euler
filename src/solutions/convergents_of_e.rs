27088016c121c6ec39a8e30fa4474b4d5853a809
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-23 23:20:57 +0530

    Solved properly.
    Removed brackets in `match` arms containing ranges.

--- src/solutions/convergents_of_e.rs
+++ src/solutions/convergents_of_e.rs
@@ -1,16 +1,31 @@
 use crate::utils;
 
-/// Determine the convergent at the specified index of the continued fraction
-/// representation of Euler's number. Index
+/// Find the continued fraction convergent of Euler's number at the specified
+/// index.
 ///
-/// * `idx` - Current index. Callers must always set this to 2.
-/// * `idx_max` - Index of the convergent. Must be at least 2.
-fn gen(idx: i32, idx_max: i32) -> utils::Fraction {
+/// * `idx_max` - Index of the convergent.
+///
+/// -> Value of the convergent; 0 if the index is non-positive.
+fn generate_continued_fraction(idx_max: i32) -> utils::Fraction {
+    match idx_max {
+        ..=0 => utils::Fraction::from(0, 1),
+        1 => utils::Fraction::from(2, 1),
+        _ => &generate_continued_fraction_(2, idx_max) + 2,
+    }
+}
+
+/// Helper to calculate the continued fraction convergent of Euler's number.
+///
+/// * `idx` - Current index.
+/// * `idx_max` - Index of the convergent.
+///
+/// -> Value of the convergent.
+fn generate_continued_fraction_(idx: i32, idx_max: i32) -> utils::Fraction {
     let addend = if idx % 3 == 0 { idx / 3 * 2 } else { 1 };
     let mut fraction = if idx == idx_max {
         utils::Fraction::from(0, 1)
     } else {
-        gen(idx + 1, idx_max)
+        generate_continued_fraction_(idx + 1, idx_max)
     };
     fraction += addend;
     fraction.invert();
@@ -18,8 +33,8 @@ fn gen(idx: i32, idx_max: i32) -> utils::Fraction {
 }
 
 pub fn solve() -> i64 {
-    let f = &gen(2, 100) + 2;
-    println!("{}", f);
+    let sum = generate_continued_fraction(100).sum().0;
 
-    0
+    assert_eq!(sum, 272);
+    sum
 }
