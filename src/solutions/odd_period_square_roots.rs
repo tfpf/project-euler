54c0e02160f10179c91c544e24913263750be11c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-23 18:36:51 +0530

    Solved.

--- src/solutions/odd_period_square_roots.rs
+++ src/solutions/odd_period_square_roots.rs
@@ -1,3 +1,34 @@
+/// Determine the period of the continued fraction representation of the
+/// square root of the given number.
+///
+/// * `value` - Number to analyse.
+///
+/// -> Continued fraction period; 0 if the square root is an integer.
+fn sqrt_continued_fraction_period(value: i64) -> usize {
+    let froot = (value as f64).sqrt();
+    if froot.fract() == 0.0 {
+        return 0;
+    }
+
+    // `numerator_integer` is the integer which gets subtracted from `froot` in
+    // the numerator of an intermediate fraction.
+    let (mut numerator_integer, mut denominator) = (froot as i64, 1);
+    for period in 1.. {
+        denominator = (value - numerator_integer.pow(2)) / denominator;
+        let subtrahend = ((froot + numerator_integer as f64) / denominator as f64) as i64;
+        numerator_integer = denominator * subtrahend - numerator_integer;
+        if denominator == 1 {
+            return period;
+        }
+    }
+    unreachable!();
+}
+
 pub fn solve() -> i64 {
-    0
+    let result = (2..=10000)
+        .filter(|&value| sqrt_continued_fraction_period(value) % 2 == 1)
+        .count();
+
+    assert_eq!(result, 1322);
+    result as i64
 }
