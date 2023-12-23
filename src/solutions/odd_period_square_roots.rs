901d19d34c1453d478300b9c6a5401b4460ce5b8
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-23 19:25:26 +0530

    Major optimisation.

--- src/solutions/odd_period_square_roots.rs
+++ src/solutions/odd_period_square_roots.rs
@@ -11,11 +11,13 @@ fn sqrt_continued_fraction_period(value: i64) -> usize {
     }
 
     // `numerator_integer` is the integer which gets subtracted from `froot` in
-    // the numerator of an intermediate fraction.
-    let (mut numerator_integer, mut denominator) = (froot as i64, 1);
+    // the numerator of an intermediate fraction. (We actually subtract it from
+    // from `iroot` as an optimisation here.)
+    let iroot = froot as i64;
+    let (mut numerator_integer, mut denominator) = (iroot, 1);
     for period in 1.. {
         denominator = (value - numerator_integer.pow(2)) / denominator;
-        let subtrahend = ((froot + numerator_integer as f64) / denominator as f64) as i64;
+        let subtrahend = (iroot + numerator_integer) / denominator;
         numerator_integer = denominator * subtrahend - numerator_integer;
         if denominator == 1 {
             return period;
