3cecd46a54dbdab8dfabd7d3be32e2f48f10dedd
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-27 22:10:19 +0530

    Tried memoisation, but still slow and not working.

--- src/solutions/digit_factorial_chains.rs
+++ src/solutions/digit_factorial_chains.rs
@@ -1,21 +1,53 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    const FACTORIAL: [i64; 10] = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+    let mut chain_len = [0; 1000000];
+    const FACTORIAL: [usize; 10] = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
     let count = (1..1000000)
         .filter(|&num| {
-            let mut num = num;
-            let mut nums = std::collections::HashSet::new();
+            // Already calculated earlier?
+            if chain_len[num] != 0 {
+                return chain_len[num] == 60;
+            }
+
+            let mut curr = num;
+            let (mut nums_set, mut nums_vec) = (std::collections::HashSet::new(), vec![curr]);
             for idx in 1..=60 {
-                nums.insert(num);
-                num = utils::Digits::new(num)
+                nums_set.insert(curr);
+                nums_vec.push(curr);
+                curr = utils::Digits::new(curr as i64)
                     .map(|digit| FACTORIAL[digit as usize])
                     .sum();
-                if nums.contains(&num) {
-                    return idx == 60;
+
+                // Reached a number from a previous chain?
+                if curr < chain_len.len() && chain_len[curr] != 0 {
+                    for (&n, length) in nums_vec.iter().rev().zip((0..chain_len[curr]).rev()) {
+                        if n < chain_len.len() {
+                            chain_len[n] = length;
+                        }
+                    }
+                    break;
+                }
+
+                // Reached a number from the current chain?
+                if nums_set.contains(&curr) {
+                    for (&n, length) in nums_vec.iter().zip((0..=idx).rev()) {
+                        if n < chain_len.len() {
+                            chain_len[n] = length;
+                        }
+                        if n == curr {
+                            break;
+                        }
+                    }
+                    for &n in nums_vec.iter().rev().take_while(|&&n| n != curr) {
+                        if n < chain_len.len() {
+                            chain_len[n] = chain_len[curr];
+                        }
+                    }
+                    break;
                 }
             }
-            false
+            chain_len[num] == 60
         })
         .count();
 
