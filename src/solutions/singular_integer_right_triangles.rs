66acc4331ec6b8b9a942478d24ed7af9e0ba3892
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-29 22:30:55 +0530

    Rewrote part of `PythagoreanTriplets` with Rusty code.

--- src/solutions/singular_integer_right_triangles.rs
+++ src/solutions/singular_integer_right_triangles.rs
@@ -1,3 +1,11 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let result = (12..=1500000)
+        .step_by(2)
+        .filter(|&perimeter| utils::PythagoreanTriplets::new(perimeter).count() == 1)
+        .count();
+
+    assert_eq!(result, 161667);
+    result as i64
 }
