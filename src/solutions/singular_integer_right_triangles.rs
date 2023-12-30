2260773b3acb10a32152095445fd20fb1bca666e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-30 14:12:34 +0530

    Further optimised: replaced `i32` cache by `u8` cache.

--- src/solutions/singular_integer_right_triangles.rs
+++ src/solutions/singular_integer_right_triangles.rs
@@ -1,21 +1,27 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let mut triangles = [0; 1500001];
+    let mut triangles = vec![0u8; 1500001];
     const PERIMETER_MAX: usize = 1500000;
     const SEMIPERIMETER_MAX: usize = PERIMETER_MAX / 2;
+
+    // Find all primitive triplets with sum less than or equal to the maximum.
     for m in 2..=(SEMIPERIMETER_MAX as f64).sqrt() as usize {
-        for n in (1 + m % 2..m).step_by(2) {
-            if utils::gcd(m as i64, n as i64) != 1 {
-                continue;
-            }
+        for n in (1 + m % 2..m)
+            .step_by(2)
+            .filter(|&n| utils::gcd(m as i64, n as i64) == 1)
+        {
             let perimeter = 2 * m * (m + n);
-            for p in (perimeter..).step_by(perimeter).take_while(|&p| p <= PERIMETER_MAX) {
+            // Increment the counter for it and its multiples.
+            for p in (perimeter..)
+                .step_by(perimeter)
+                .take_while(|&p| p <= PERIMETER_MAX)
+            {
                 triangles[p] += 1;
             }
         }
     }
-    let result = triangles.iter().filter(|&&t| t == 1).count();
+    let result = triangles.iter().filter(|&&elem| elem == 1).count();
 
     assert_eq!(result, 161667);
     result as i64
