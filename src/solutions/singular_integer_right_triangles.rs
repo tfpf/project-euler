0d1570f5f26add4031b9f142f9b76867543f344b
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2023-12-30 13:05:12 +0530

    Faster solution by exploiting triplet generator.
    The Pythagorean triplet generating algorithm can be adapted to generate all triplets with a sum less than or equal to a given number instead of those with a sum equal to the same (which is what `utils::PythagoreanTriplets` does). Consequently, running the adapted algorithm for 1500000 is faster than running the latter algorithm for all numbers from 12 to 1500000.

--- src/solutions/singular_integer_right_triangles.rs
+++ src/solutions/singular_integer_right_triangles.rs
@@ -1,10 +1,21 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let result = (12..=1500000)
-        .step_by(2)
-        .filter(|&perimeter| utils::PythagoreanTriplets::new(perimeter).take(2).count() == 1)
-        .count();
+    let mut triangles = [0; 1500001];
+    const PERIMETER_MAX: usize = 1500000;
+    const SEMIPERIMETER_MAX: usize = PERIMETER_MAX / 2;
+    for m in 2..=(SEMIPERIMETER_MAX as f64).sqrt() as usize {
+        for n in (1 + m % 2..m).step_by(2) {
+            if utils::gcd(m as i64, n as i64) != 1 {
+                continue;
+            }
+            let perimeter = 2 * m * (m + n);
+            for p in (perimeter..).step_by(perimeter).take_while(|&p| p <= PERIMETER_MAX) {
+                triangles[p] += 1;
+            }
+        }
+    }
+    let result = triangles.iter().filter(|&&t| t == 1).count();
 
     assert_eq!(result, 161667);
     result as i64
