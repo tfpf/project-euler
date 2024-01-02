68a88074863ddb805dc89ac26cd121f1641bd913
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-02 17:32:11 +0530

    Rustified trial by division prime test.

--- src/utils.rs
+++ src/utils.rs
@@ -77,12 +77,9 @@ fn is_prime_large_test() {
 fn is_prime_tbd(num: i64) -> bool {
     // No need to search for composite factors. We'll find prime factors (if
     // any) faster.
-    for potential_prime in PotentialPrimes::new((num as f64).sqrt() as i64).skip(3) {
-        if num % potential_prime == 0 {
-            return false;
-        }
-    }
-    true
+    PotentialPrimes::new((num as f64).sqrt() as i64)
+        .skip(3)
+        .all(|potential_prime| num % potential_prime != 0)
 }
 
 /// Check whether the given number is prime using the Miller-Rabin test.
