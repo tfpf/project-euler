0f55f6a6f70d3f9772f32c5764db1eccd205a3d6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-07 09:18:56 +0530

    Moved unwanted functions from `utils.rs` to solution.

--- src/solutions/counting_sundays.rs
+++ src/solutions/counting_sundays.rs
@@ -1,6 +1,36 @@
 use crate::utils;
 
-pub fn solve() -> i64 {
+/// Check for leap years.
+///
+/// * `year`
+///
+/// -> Whether the input is a leap year.
+fn is_leap(year: i64) -> bool {
+    year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+}
+
+/// Determine the number of days in the given month of the given year.
+///
+/// * `year`
+/// * `month` - Number from 1 to 12.
+///
+/// -> Number of days, or 0 if the month is invalid.
+pub fn days_in(year: i64, month: i64) -> i64 {
+    match month {
+        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
+        4 | 6 | 9 | 11 => 30,
+        2 => {
+            if is_leap(year) {
+                29
+            } else {
+                28
+            }
+        }
+        _ => 0,
+    }
+}
+
+fn solve() -> i64 {
     // If 1 January 1900 was a Monday, then 1 January 1901 was a Tuesday.
     // Represent each day as an offset from Sunday. (e.g. Tuesday is 2.)
     let mut day = 2;
--- src/utils.rs
+++ src/utils.rs
@@ -165,36 +165,6 @@ fn gcd_test() {
     assert_eq!(coprime_pairs, 58752000);
 }
 
-/// Check for leap years.
-///
-/// * `year`
-///
-/// -> Whether the input is a leap year.
-pub fn is_leap(year: i64) -> bool {
-    year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-}
-
-/// Determine the number of days in the given month of the given year.
-///
-/// * `year`
-/// * `month` - Number from 1 to 12.
-///
-/// -> Number of days, or 0 if the month is invalid.
-pub fn days_in(year: i64, month: i64) -> i64 {
-    match month {
-        1 | 3 | 5 | 7 | 8 | 10 | 12 => 31,
-        4 | 6 | 9 | 11 => 30,
-        2 => {
-            if is_leap(year) {
-                29
-            } else {
-                28
-            }
-        }
-        _ => 0,
-    }
-}
-
 /// Find the length of the repeating part of the decimal representation of the
 /// reciprocal of a prime number.
 ///
