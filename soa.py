05d6ad823eb2d20d9eed650f70d568568c2bad3d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-12 22:04:26 +0530

    Implemented algorithms 3.1 and 4.1 correctly.
    The mistake I was making was in step 3 of algorithm 4.1. This step basically reverses the last iteration of the previous step. I had not done this reversal correctly.

--- soa.py
+++ soa.py
@@ -5,40 +5,49 @@ import math
 
 
 def algorithm_31(delta):
-    for (x, y, k) in algorithm_41(delta):
+    for (x, y, k) in algorithm_41_(delta):
         sieve[60 * k + delta] ^= True
 
-def algorithm_41(delta):
+def algorithm_41_(delta):
     for (f, g) in itertools.product(range(1, 16), range(1, 31)):
-        if (4 * f ** 2 + g ** 2) % 60 != delta:
-            continue
-        print('Found fg:', delta, f, g)
-        (x, y0, k0) = (f, g, (4 * f ** 2 + g ** 2 - delta) // 60)
-        while k0 < B:
-            (k0, x) = (k0 + 2 * x + 15, x + 15)
-        print(x, y0, k0)
-        while True:
-            (x, k0) = (x - 15, k0 - 2 * x - 15)
-            if x <= 0:
-                break
-            while k0 < 0:
-                (k0, y0) = (k0 + y0 + 15, y0 + 30)
-            (k, y) = (k0, y0)
-            while k < B:
-                yield (x, y, k)
-                (k, y) = (k + y + 15, y + 30)
+        if delta == (4 * f ** 2 + g ** 2) % 60:
+            print(f'\033[1;31mFound {delta=} {f=} {g=}\033[m')
+            for item in algorithm_41(delta, f, g):
+                yield item
+
+def algorithm_41(delta, f, g):
+    (x, y0, k0) = (f, g, (4 * f ** 2 + g ** 2 - delta) // 60)
+    print(f'  Starting with {x=} {y0=} {k0=}')
+    while k0 < B:
+        (k0, x) = (k0 + 2 * x + 15, x + 15)
+    print(f'  Changed to {x=} {y0=} {k0=}')
+    while True:
+        (k0, x) = (k0 - 2 * x + 15, x - 15)
+        print(f'    Moved left {x=} {y0=} {k0=}')
+        if x <= 0:
+            break
+        while k0 < 0:
+            (k0, y0) = (k0 + y0 + 15, y0 + 30)
+            print(f'      Moved up {x=} {y0=} {k0=}')
+        print(f'    Reached {x=} {y0=} {k0=}')
+        (k, y) = (k0, y0)
+        while k < B:
+            print(f'      Got {x=} {y=} {k=}')
+            yield (x, y, k)
+            (k, y) = (k + y + 15, y + 30)
 
 
-N = 1199
+N = 130
+N -= N % 60 + 1
 B = math.ceil((N + 1) / 60)
 sieve = [False] * (N + 1)
 print(f'{N=} {B=}')
 for delta in range(60):
     if delta in {1, 13, 17, 29, 37, 41, 49, 53}:
         algorithm_31(delta)
-
-for item in itertools.islice(enumerate(sieve), 10000000000):
-    print(item)
+for (num, prime) in itertools.islice(enumerate(sieve), 10000000000):
+    if prime:
+        print(num)
 
 
 # for (f, g) in itertools.product(range(1, 16), range(1, 31)):
