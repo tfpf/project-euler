795611216416242244ed7d358c9558e3330fc268
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-19 18:15:38 +0530

    Formatted.

--- src/solutions/prime_power_triples.rs
+++ src/solutions/prime_power_triples.rs
@@ -7,8 +7,16 @@ pub fn solve() -> i64 {
     // element.
     let mut p234sum_expressible = vec![0u64; 781250];
     for a in primes.iter().map(|prime| prime.pow(2)) {
-        for b in primes.iter().map(|prime| prime.pow(3)).take_while(|b| a + b < 50000000) {
-            for c in primes.iter().map(|prime| prime.pow(4)).take_while(|c| a + b + c < 50000000) {
+        for b in primes
+            .iter()
+            .map(|prime| prime.pow(3))
+            .take_while(|b| a + b < 50000000)
+        {
+            for c in primes
+                .iter()
+                .map(|prime| prime.pow(4))
+                .take_while(|c| a + b + c < 50000000)
+            {
                 let num = (a + b + c) as usize;
                 p234sum_expressible[num / 64] |= 1 << (num % 64);
             }
