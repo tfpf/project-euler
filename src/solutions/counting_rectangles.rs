d796cfe5c2f8fb2adaa1fc6a4e8ea711094d7646
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-19 14:25:31 +0530

    Working but slow solution.

--- src/solutions/counting_rectangles.rs
+++ src/solutions/counting_rectangles.rs
@@ -1,1 +1,16 @@
-pub fn solve()->i64{0}
+pub fn solve() -> i64 {
+    let (mut best_a, mut best_b, mut best_off) = (0, 0, i64::MAX);
+    for a in (0..6000).rev() {
+        for b in 0..6000 {
+            let rectangles: i64 = a * (a + 1) * b * (b + 1) / 4;
+            let off = (2000000 - rectangles).abs();
+            if off < best_off {
+                (best_a, best_b, best_off) = (a, b, off);
+            }
+        }
+    }
+    println!("{} {}", best_a, best_b);
+    let result = best_a * best_b;
+
+    result
+}
