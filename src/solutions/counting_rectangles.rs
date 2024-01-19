529eb37e7143d4ac115acad38d0b3a8d41d63126
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-19 16:29:25 +0530

    Made `Polygonal::invert` return `Result` instead of `Option`.
    Even if the index is not an integer, it may still help find out what the nearest index might be. Actually, I made this change to try another approach in P85 (keep increasing `a` and determine what value `b` must take so that the number of rectangles is 2000000), but ended up not using it because I am too lazy to figure out how to make it work.

--- src/solutions/counting_rectangles.rs
+++ src/solutions/counting_rectangles.rs
@@ -4,12 +4,12 @@ pub fn solve() -> i64 {
     // The number of rectangles in a rectangular grid measuring `a` units by
     // `b` units is obtained by multiplying the sum of all positive integers
     // till `a` and the sum of all positive integers till `b`. Said product is
-    // approximately `a.pow(2) * b.pow(2) / 4`, which must be close to 2000000.
-    // Hence, we can make an educated guess about the range to test `a` and `b`
-    // for.
+    // of the order of `a.pow(2) * b.pow(2) / 4`, which must be close to
+    // 2000000. Hence, we can make an educated guess about the range to test
+    // `a` and `b` for.
     let mut result = (0, 0, i64::MAX);
-    for (a, sum_till_a) in (1..).zip(utils::Polygonal::new(3)).take(500) {
-        for (b, sum_till_b) in (1..).zip(utils::Polygonal::new(3)).take(500) {
+    for (a, sum_till_a) in (1..).zip(utils::Polygonal::new(3)).take(200) {
+        for (b, sum_till_b) in (1..).zip(utils::Polygonal::new(3)).take(200) {
             let rectangles = sum_till_a * sum_till_b;
             let off = 2000000 - rectangles;
             let off_abs = off.abs();
