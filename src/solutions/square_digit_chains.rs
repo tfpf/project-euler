264b83a894bdf923a989ac61ed3c86abc94714b6
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-21 19:14:58 +0530

    Optimised: running time down from 720 ms to 240 ms.

--- src/solutions/square_digit_chains.rs
+++ src/solutions/square_digit_chains.rs
@@ -1,11 +1,12 @@
 use crate::utils;
 
 pub fn solve() -> i64 {
-    let mut chain_stuck = vec![0u8; 10000000];
+    // The greatest possible sum of digit squares in this problem is 567. Find
+    // out how many numbers from 1 to 567 get stuck at 89.
+    let mut chain_stuck = vec![0u8; 568];
     chain_stuck[1] = 1;
     chain_stuck[89] = 89;
-
-    let result = (1..chain_stuck.len()).filter(|&num| {
+    let difficult = (1..568).filter(|&num| {
         let mut chain = vec![];
         let mut num = num;
         while chain_stuck[num] == 0 {
@@ -17,6 +18,15 @@ pub fn solve() -> i64 {
         }
         chain_stuck[num] == 89
     }).count();
+
+    // Do the same for the rest of the numbers. This is simpler, because we can
+    // just check where their sum of digit squares gets stuck, since it was
+    // calculated above.
+    let easy = (568..10000000).filter(|&num| {
+        let num = utils::Digits::new(num as i64).map(|digit| digit.pow(2)).sum::<i64>() as usize;
+        chain_stuck[num] == 89
+    }).count();
+    let result = difficult + easy;
 
     assert_eq!(result, 8581146);
     result as i64
