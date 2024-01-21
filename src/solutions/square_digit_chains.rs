3a38e8c9579486c3d57260cb108efb0806c48f4e
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-21 19:15:31 +0530

    Formatted.

--- src/solutions/square_digit_chains.rs
+++ src/solutions/square_digit_chains.rs
@@ -6,26 +6,34 @@ pub fn solve() -> i64 {
     let mut chain_stuck = vec![0u8; 568];
     chain_stuck[1] = 1;
     chain_stuck[89] = 89;
-    let difficult = (1..568).filter(|&num| {
-        let mut chain = vec![];
-        let mut num = num;
-        while chain_stuck[num] == 0 {
-            chain.push(num);
-            num = utils::Digits::new(num as i64).map(|digit| digit.pow(2)).sum::<i64>() as usize;
-        }
-        for chain_node in chain {
-            chain_stuck[chain_node] = chain_stuck[num];
-        }
-        chain_stuck[num] == 89
-    }).count();
+    let difficult = (1..568)
+        .filter(|&num| {
+            let mut chain = vec![];
+            let mut num = num;
+            while chain_stuck[num] == 0 {
+                chain.push(num);
+                num = utils::Digits::new(num as i64)
+                    .map(|digit| digit.pow(2))
+                    .sum::<i64>() as usize;
+            }
+            for chain_node in chain {
+                chain_stuck[chain_node] = chain_stuck[num];
+            }
+            chain_stuck[num] == 89
+        })
+        .count();
 
     // Do the same for the rest of the numbers. This is simpler, because we can
     // just check where their sum of digit squares gets stuck, since it was
     // calculated above.
-    let easy = (568..10000000).filter(|&num| {
-        let num = utils::Digits::new(num as i64).map(|digit| digit.pow(2)).sum::<i64>() as usize;
-        chain_stuck[num] == 89
-    }).count();
+    let easy = (568..10000000)
+        .filter(|&num| {
+            let num = utils::Digits::new(num as i64)
+                .map(|digit| digit.pow(2))
+                .sum::<i64>() as usize;
+            chain_stuck[num] == 89
+        })
+        .count();
     let result = difficult + easy;
 
     assert_eq!(result, 8581146);
