41d09772a7d7622d5f2c4d03d3a4cce4babecd82
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-22 22:30:40 +0530

    Solved.

--- src/solutions/large_non_mersenne_prime.rs
+++ src/solutions/large_non_mersenne_prime.rs
@@ -1,3 +1,9 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let modulus = 10i64.pow(10);
+    let result = (utils::pow(2, 7830457, modulus) * 28433 + 1) % modulus;
+
+    assert_eq!(result, 8739992577);
+    result
 }
