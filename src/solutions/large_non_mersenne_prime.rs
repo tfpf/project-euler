46a3095b955f4bb5759872ba01b018871250a36f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-22 22:43:12 +0530

    Rewrote in same form the question writes the number.

--- src/solutions/large_non_mersenne_prime.rs
+++ src/solutions/large_non_mersenne_prime.rs
@@ -2,7 +2,7 @@ use crate::utils;
 
 pub fn solve() -> i64 {
     let modulus = 10i64.pow(10);
-    let result = (utils::pow(2, 7830457, modulus) * 28433 + 1) % modulus;
+    let result = (28433 * utils::pow(2, 7830457, modulus) + 1) % modulus;
 
     assert_eq!(result, 8739992577);
     result
