7aa20b7ff9cdf5b69aad2c071bf1d8458235fb75
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-26 00:13:17 +0530

    This should really have worked, but it is not working.
    Gives the correct answer for 7. Cannot find any numbers for 8.

--- src/solutions/prime_digit_replacements.rs
+++ src/solutions/prime_digit_replacements.rs
@@ -1,32 +1,31 @@
 use crate::utils;
 
-fn replacements_are_prime(prime: i64, sieve: &utils::SieveOfEratosthenes) -> bool {
-    for difference in [11, 101, 110, 1001, 1010, 1100, 10001, 10010, 10100, 11000, 100001, 100010, 100100, 101000, 110000, 1000001, 1000010, 1000100, 1001000, 1010000, 1100000] {
-        if difference > prime {
-            break;
-        }
-        if (0..=9).filter(|digit| {
-            let num = prime + digit * difference;
-            utils::is_prime(num)
-        }).count() >= 8 {
-            return true;
-        }
-    }
-
-
-
-
-    false
-}
-
 pub fn solve() -> i64 {
-    let sieve = utils::SieveOfEratosthenes::new(100000000);
+    const LIMIT: usize = 1000000000;
+    let sieve = utils::SieveOfAtkin::new(LIMIT);
     for prime in sieve.iter() {
-        if replacements_are_prime(prime, &sieve) {
-            println!("{}", prime);
+        let idx_digits = utils::Digits::new(prime)
+            .enumerate()
+            .filter(|&(_, digit)| digit <= 2)
+            .collect::<Vec<(usize, i64)>>();
+        for &(i, idigit) in &idx_digits {
+            for &(j, jdigit) in idx_digits.iter().take_while(|(j, _)| *j < i) {
+                if idigit == jdigit {
+                    let jump = 10usize.pow(i as u32) + 10usize.pow(j as u32);
+                    let candidates = 10 - idigit as usize;
+                    if (prime as usize..)
+                        .step_by(jump)
+                        .take(candidates)
+                        .filter(|&num| sieve.is_prime(num))
+                        .count()
+                        == 7
+                    {
+                        println!("{}", prime);
+                    }
+                }
+            }
         }
     }
-    // After finding a prime number, check whether the numbers obtained by
-    // adding 11, 22, 33 and so on (or 101, 202, 303 and so on) are prime.
+
     0
 }
