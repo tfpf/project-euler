8f22e82994cfe26c8ab63eeb560df3fa7cd5ead5
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-26 10:39:20 +0530

    Modified using hint from PE thread.

--- src/solutions/prime_digit_replacements.rs
+++ src/solutions/prime_digit_replacements.rs
@@ -1,31 +1,57 @@
 use crate::utils;
 
+/// Find an 8-family of prime numbers which have the same digits, except for
+/// those in some fixed positions; said positions have the same digit.
+///
+/// The number of fixed positions must be 3, 6 or 9. Otherwise, at least one
+/// of the 8 numbers in the family will be divisible by 3. We get lucky with
+/// assuming that it is 3.
+///
+/// -> Smallest member of the family.
 fn prime_digit_replacements() -> i64 {
     const LIMIT: usize = 1000000;
     let sieve = utils::SieveOfAtkin::new(LIMIT);
     for prime in sieve.iter() {
-        let idx_digits = utils::Digits::new(prime)
+        // Break the number into its digits. Note the position/positions each
+        // appears at.
+        let mut digitpos: [Vec<usize>; 3] = [vec![], vec![], vec![]];
+        for (position, digit) in utils::Digits::new(prime)
             .enumerate()
+            // If the digit is 3 or more, we cannot have an 8-family.
             .filter(|&(_, digit)| digit <= 2)
-            .collect::<Vec<(usize, i64)>>();
-        for &(i, idigit) in &idx_digits {
-            for &(j, jdigit) in idx_digits.iter().take_while(|(j, _)| *j < i) {
-                for &(k, kdigit) in idx_digits.iter().take_while(|(k, _)| *k < j) {
-                    if idigit == jdigit && jdigit == kdigit {
-                        let jump =
-                            10usize.pow(i as u32) + 10usize.pow(j as u32) + 10usize.pow(k as u32);
-                        let candidates = 10 - idigit as usize;
-                        if (prime as usize..)
-                            .step_by(jump)
-                            .take(candidates)
-                            .filter(|&num| sieve.is_prime(num))
-                            .count()
-                            == 8
-                        {
-                            return prime;
-                        }
-                    }
-                }
+        {
+            digitpos[digit as usize].push(position);
+        }
+
+        // Which digit can we replace?
+        for (digit, positions) in digitpos.iter().enumerate() {
+            match positions.len() {
+                3 | 6 | 9 => (),
+                _ => continue,
+            }
+
+            // Replace all occurrences of this digit with successive digits.
+            // Find the number which, upon repeatedly adding to the current
+            // prime, will have said effect.
+            let jump = positions
+                .iter()
+                .map(|&position| 10usize.pow(position as u32))
+                .sum::<usize>();
+            // How many times should it be added? Until the replaced digit
+            // becomes 9.
+            let candidates = 9 - digit as usize;
+
+            if (prime as usize..)
+                .step_by(jump)
+                // The first number is already known to be prime, so ignore it.
+                // Out of the remaining numbers, 7 should be prime.
+                .skip(1)
+                .take(candidates)
+                .filter(|&num| sieve.is_prime(num))
+                .count()
+                == 7
+            {
+                return prime;
             }
         }
     }
