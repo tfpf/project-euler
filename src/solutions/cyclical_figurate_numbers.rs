1726742b7f3bf427cebaf8fbc42a4d9a97ef4ebb
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-01-27 11:11:05 +0530

    Something is working, though not correct.

--- src/solutions/cyclical_figurate_numbers.rs
+++ src/solutions/cyclical_figurate_numbers.rs
@@ -1,40 +1,58 @@
 use crate::utils;
 
-// fn set_polygonal(is_polygonal: &mut [u8], sides: i64, num: i64) {
-//     is_polygonal[num as usize - 1000] |= 1 << (sides - 3);
-// }
-
-fn generate_cyclical(is_polygonal: &[u8], mask: u8, cyclical: &mut Vec<i64>) {
+fn generate_cyclical(numbitfield: &Vec<(i64, u8)>, mask: u8, cyclical: &mut Vec<i64>) -> bool {
     if mask == 0 {
         return cyclical.first().unwrap() / 100 == cyclical.last().unwrap() % 100;
     }
-
-    let (begin, end) = if mask == 63 {
-        (1000, 10000)
+    let (begin_val, begin_idx, difference) = if cyclical.is_empty() {
+        (0, 0, i64::MAX)
     } else {
-        let begin = cyclical.last().unwrap() % 100 * 100;
-        let end = std::cmp::min(begin + 100, 10000);
-        (begin, end)
+        let begin_val = cyclical.last().unwrap() % 100 * 100;
+        let begin_idx = match numbitfield.binary_search(&(begin_val, 0)) {
+            Ok(begin_idx) => begin_idx,
+            Err(begin_idx) => begin_idx,
+        };
+        (begin_val, begin_idx, 100)
     };
-    for num in (begin..end).filter(|num| get_polygonal) {
+    for &(num, bitfield) in numbitfield[begin_idx..]
+        .iter()
+        .take_while(|&&(num, _)| num < begin_val + difference)
+    {
+        cyclical.push(num);
+        for shift in 0..6 {
+            if bitfield >> shift & 1 == 1
+                && mask >> shift & 1 == 1
+                && generate_cyclical(numbitfield, mask & !(1 << shift), cyclical)
+            {
+                return true;
+            }
+        }
+        cyclical.pop();
     }
+    false
 }
 
 pub fn solve() -> i64 {
-    let mut is_polygonal = vec![0u8; 10000];
+    let mut numbitfield = std::collections::BTreeMap::new();
     for sides in 3..=8 {
         for num in utils::Polygonal::new(sides)
             .skip_while(|&num| num < 1000)
             .take_while(|&num| num < 10000)
         {
-            is_polygonal[num as usize] |= 1 << (sides - 3);
-            // Turn this into a hashmap? Iterate over it and keep clearing mask bits?
-            // set_polygonal(&mut is_polygonal, sides, num);
+            let curr_mask = 1u8 << (sides - 3);
+            let prev_mask = match numbitfield.get(&num) {
+                Some(&bitfield) => bitfield,
+                None => 0,
+            };
+            numbitfield.insert(num, prev_mask | curr_mask);
         }
     }
+    let numbitfield = numbitfield.into_iter().collect::<Vec<(i64, u8)>>();
+    println!("{:?}", numbitfield);
 
-    let cyclical = vec![];
-    generate_cyclical(&is_polygonal, 63, &mut cyclical);
+    let mut cyclical = vec![];
+    generate_cyclical(&numbitfield, 63, &mut cyclical);
+    println!("{:?}", cyclical);
 
     0
 }
