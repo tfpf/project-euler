bd893ba66c857aa6f6a5a9f755aa8b726a5df00a
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-03 23:40:38 +0530

    Added `totient` function.
    Too slow, though.

--- src/solutions/totient_permutation.rs
+++ src/solutions/totient_permutation.rs
@@ -1,3 +1,12 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
+    for num in 2..10i64.pow(7) {
+        let t = utils::totient(num);
+        if utils::digits_frequencies(num) == utils::digits_frequencies(t) {
+            println!("{} {}", num as f64 / t as f64, num);
+        }
+    }
+
     0
 }
