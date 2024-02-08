6d36512d92989a4805920b42149b6fbad58efbb5
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-08 23:15:11 +0530

    Updated comments.

--- src/solutions/ordered_fractions.rs
+++ src/solutions/ordered_fractions.rs
@@ -2,12 +2,12 @@ pub fn solve() -> i64 {
     let mut fraction = (3, 7);
     let result = (2..=1000000)
         .filter_map(|_| {
-            // Produce a fraction equal in value to the target. Find a smaller
-            // fraction with the same denominator.
+            // The difference between any two consecutive fractions is 1 over
+            // the product of their denominators. Hence, produce a fraction
+            // equal in value to the target, and find a smaller fraction with
+            // the same denominator. Then try to divide throughout by 7.
             fraction = (fraction.0 + 3, fraction.1 + 7);
             let (numerator, denominator) = (fraction.0 - 1, fraction.1);
-            // Their GCD can only be 7. Not the multiplier, because we
-            // subtracted 1 from the numerator.
             if numerator % 7 == 0 && denominator % 7 == 0 {
                 Some(numerator / 7)
             } else {
