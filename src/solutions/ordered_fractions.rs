00df77132057a5dce76eb1b248030b71e2145132
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-08 22:57:19 +0530

    Working (slow).

--- src/solutions/ordered_fractions.rs
+++ src/solutions/ordered_fractions.rs
@@ -1,3 +1,21 @@
+use crate::utils;
+
 pub fn solve() -> i64 {
-    0
+    let mut fraction = (3, 7);
+    let result = (2..=1000000)
+        .filter_map(|_| {
+            fraction = (fraction.0 + 3, fraction.1 + 7);
+            let (numerator, denominator) = (fraction.0 - 1, fraction.1);
+            let g = utils::gcd(numerator, denominator);
+            if denominator / g <= 1000000 {
+                Some(numerator / g)
+            } else {
+                None
+            }
+        })
+        .last()
+        .unwrap();
+
+    assert_eq!(result, 428570);
+    result
 }
