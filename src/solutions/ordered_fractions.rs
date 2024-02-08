1e97833a6b56d30f26bee3cca950faf9d3022aff
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-08 23:10:14 +0530

    Optimised.

--- src/solutions/ordered_fractions.rs
+++ src/solutions/ordered_fractions.rs
@@ -1,14 +1,15 @@
-use crate::utils;
-
 pub fn solve() -> i64 {
     let mut fraction = (3, 7);
     let result = (2..=1000000)
         .filter_map(|_| {
+            // Produce a fraction equal in value to the target. Find a smaller
+            // fraction with the same denominator.
             fraction = (fraction.0 + 3, fraction.1 + 7);
             let (numerator, denominator) = (fraction.0 - 1, fraction.1);
-            let g = utils::gcd(numerator, denominator);
-            if denominator / g <= 1000000 {
-                Some(numerator / g)
+            // Their GCD can only be 7. Not the multiplier, because we
+            // subtracted 1 from the numerator.
+            if numerator % 7 == 0 && denominator % 7 == 0 {
+                Some(numerator / 7)
             } else {
                 None
             }
