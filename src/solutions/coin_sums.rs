ee196ba8e6909c7bb387617e1e1d600c0c87840f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-11 13:13:37 +0530

    Clippy lints.

--- src/solutions/coin_sums.rs
+++ src/solutions/coin_sums.rs
@@ -1,16 +1,20 @@
 pub fn solve() -> i64 {
     let denominations = [1, 2, 5, 10, 20, 50, 100, 200];
 
-    // Any element in this matrix be the number of ways to obtain a sum equal
-    // to the column index using coins at indices less than or equal to the row
-    // index.
+    // Any element in this matrix shall be the number of ways to obtain a sum
+    // equal to the column index using coins at indices less than or equal to
+    // the row index. Note that there is only one way to obtain a sum of 0.
+    // Hence, as an optimisation, set the first element of each row of the
+    // matrix to 1 during initialisation.
     let (rows, cols) = (denominations.len(), denominations.iter().max().unwrap() + 1);
-    let mut ways = vec![vec![0; cols]; rows];
+    let mut ways = vec![
+        std::iter::once(1)
+            .chain(std::iter::repeat(0).take(cols - 1))
+            .collect::<Vec<i32>>();
+        rows
+    ];
 
     // Bottom-up dynamic programming.
-    for idx in 0..rows {
-        ways[idx][0] = 1;
-    }
     for sum in 1..cols {
         ways[0][sum] = if sum % denominations[0] == 0 { 1 } else { 0 };
     }
