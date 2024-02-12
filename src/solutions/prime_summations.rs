abbf187c0c4cecd47ea03b34c0697e7e474cbe8d
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-12 22:00:21 +0530

    Optimised P77 solution further.

--- src/solutions/prime_summations.rs
+++ src/solutions/prime_summations.rs
@@ -2,37 +2,33 @@ use crate::utils;
 
 pub fn solve() -> i64 {
     // Approach is similar to that used for P31. The difference is that the
-    // maximum denomination is guessed, and that there is no target sum.
+    // denominations are prime numbers (how many of them to use is guessed),
+    // and that there is no target sum.
     const LIMIT: usize = 100;
     let primes = utils::SieveOfAtkin::new(LIMIT).iter().collect::<Vec<i64>>();
     let (rows, cols) = (primes.len(), LIMIT + 1);
-    let mut ways = vec![
-        std::iter::once(1)
-            .chain(std::iter::repeat(0).take(cols - 1))
-            .collect::<Vec<i64>>();
-        rows
-    ];
+
+    // This will no longer be all ones, because the number of ways to obtain a
+    // particular sum using only the first prime number depends on the parity
+    // of the sum!
+    let mut curr = (0..cols)
+        .map(|sum| if sum % primes[0] as usize == 0 { 1 } else { 0 })
+        .collect();
+    let mut prev = vec![1; cols];
 
     // Bottom-up dynamic programming.
-    for sum in 1..cols {
-        ways[0][sum] = if sum % primes[0] as usize == 0 { 1 } else { 0 };
-    }
     for idx in 1..rows {
+        (prev, curr) = (curr, prev);
         for sum in 1..cols {
-            ways[idx][sum] = ways[idx - 1][sum]
+            curr[sum] = prev[sum]
                 + if sum >= primes[idx] as usize {
-                    ways[idx][sum - primes[idx] as usize]
+                    curr[sum - primes[idx] as usize]
                 } else {
                     0
                 };
         }
     }
-    let result = ways[rows - 1]
-        .iter()
-        .enumerate()
-        .find(|(_, &count)| count >= 5000)
-        .unwrap()
-        .0;
+    let result = curr.iter().enumerate().find(|(_, &count)| count >= 5000).unwrap().0;
 
     result as i64
 }
