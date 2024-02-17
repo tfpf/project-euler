ccb9e6b2c4251c6e59ddc8074ef0b2749378fca7
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-17 10:58:15 +0530

    Moved lint ignores to the file they are required in.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,2 @@
-#![deny(clippy::all)]
-#![allow(clippy::len_without_is_empty)]
-#![allow(clippy::manual_try_fold)]
-#![allow(clippy::match_overlapping_arm)]
-#![allow(clippy::new_without_default)]
-
 pub mod solutions;
 pub mod utils;
