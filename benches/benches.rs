c1a562c18de7017ecf9a89c9e0cc2f22a8a17fff
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-17 11:46:20 +0530

    Reduced benchmark scope.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,7 +1,7 @@
 use project_euler::utils;
 
 pub fn sieve_of_atkin(c: &mut criterion::Criterion) {
-    c.bench_function("sieve_of_atkin", |b| b.iter(|| utils::SieveOfAtkin::new(100000000)));
+    c.bench_function("sieve_of_atkin", |b| b.iter(|| utils::SieveOfAtkin::new(1000000)));
 }
 
 criterion::criterion_group!(benches, sieve_of_atkin);
