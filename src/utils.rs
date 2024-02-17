f67325934ebcc818c7691f23454b86e020ac1a34
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-02-17 11:53:13 +0530

    Added sieve of Atkin benchmark. (#140)
    Converted this project from a binary to a library with the main program thinly wrapping said library, because benchmarks can only be run on library functions.

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,9 @@
+#![deny(clippy::all)]
+#![allow(clippy::len_without_is_empty)]
+#![allow(clippy::manual_try_fold)]
+#![allow(clippy::match_overlapping_arm)]
+#![allow(clippy::new_without_default)]
+
 /******************************************************************************
  * Functions.
  *****************************************************************************/
