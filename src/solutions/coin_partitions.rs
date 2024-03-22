3479f42ba058699bc90601d9580a9f06ca8c4e31
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-03-22 22:46:57 +0530

    Optimised solution using recurrence relation to calculate partitions.

--- src/solutions/coin_partitions.rs
+++ src/solutions/coin_partitions.rs
@@ -1,24 +1,47 @@
-pub fn solve() -> i64 {
-    // Approach is similar to that used for P76. The difference is that the
-    // maximum denomination is guessed and that there is no target sum.
-    let (rows, cols) = (100000, 100000);
-    let mut curr = vec![1; cols];
-    let mut prev = vec![1; cols];
+use crate::utils;
 
-    // Bottom-up dynamic programming.
-    for idx in 2..rows {
-        (prev, curr) = (curr, prev);
-        for sum in 1..cols {
-            curr[sum] = prev[sum] + if sum >= idx { curr[sum - idx] } else { 0 };
-            curr[sum] %= 1000000;
-        }
-        print!("\r{} {}", idx, curr[idx]);
-        // if idx % 512 == 0 {print!("\r{}", idx);}
-        if curr[idx] == 0 {
-            return idx as i64;
+/// Compute the number of ways to obtain each number up to the given number by
+/// adding positive integers. Essentially, implement the partition function (of
+/// number theory) for each number up to the given number. The values shall be
+/// modulo 1000000.
+///
+/// * `num` - Last number.
+///
+/// -> Array of the numbers of ways to to sum to the array index.
+pub fn partitions(num: usize) -> Vec<i32> {
+    let mut p = vec![0; num + 1];
+    p[0] = 1;
+    p[1] = 1;
+    for idx in 2..=num {
+        // We have to iterate over generalised pentagon numbers to evaluate the
+        // partition function at any value. Generate them using pentagon
+        // numbers.
+        for (i, subtrahend) in utils::Polygonal::new(5)
+            .zip(1..)
+            .flat_map(|(polygonal, offset)| [polygonal, polygonal + offset].into_iter())
+            .take_while(|&subtrahend| idx >= subtrahend as usize)
+            .enumerate()
+        {
+            let rec = p[idx - subtrahend as usize];
+            if i % 4 < 2 {
+                p[idx] += rec;
+            } else {
+                p[idx] -= rec;
+            }
+            p[idx] %= 1000000;
         }
     }
+    p
+}
 
-    assert_eq!(55374, 55374);
-    55374
+pub fn solve() -> i64 {
+    let result = partitions(100000)
+        .iter()
+        .enumerate()
+        .find(|(_, &count)| count == 0)
+        .unwrap()
+        .0;
+
+    assert_eq!(result, 55374);
+    result as i64
 }
