a27fc0e8684d0c15f0c3b887d199a91ecb685cce
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-03-23 21:14:20 +0530

    Problem 78. (#148)

--- src/main.rs
+++ src/main.rs
@@ -82,6 +82,7 @@ fn solve_and_time_one(problem_number: i32) -> bool {
         75 => solutions::singular_integer_right_triangles::solve,
         76 => solutions::counting_summations::solve,
         77 => solutions::prime_summations::solve,
+        78 => solutions::coin_partitions::solve,
         81 => solutions::path_sum_two_ways::solve,
         85 => solutions::counting_rectangles::solve,
         87 => solutions::prime_power_triples::solve,
