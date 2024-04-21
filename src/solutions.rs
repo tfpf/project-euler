88858a8e00f8d16d5c83d020970d92d7e0f5a4aa
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-04-21 19:23:24 +0530

    Reverted commit 117d770f5f5c206751742c59212c701b3956b877.

--- src/solutions.rs
+++ src/solutions.rs
@@ -78,8 +78,7 @@ pub mod thousand_digit_fibonacci_number;
 pub mod totient_maximum;
 pub mod triangular_pentagonal_and_hexagonal;
 pub mod truncatable_primes;
-mod xor_decryption;
-pub use xor_decryption::solve;
+pub mod xor_decryption;
 
 #[cfg(test)]
 mod tests {
