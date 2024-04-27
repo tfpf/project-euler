abc02b73b6c1c673cab1a412fffb09e7a3f8e66f
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-04-27 14:56:49 +0530

    Formatted.

--- src/solutions/longest_collatz_sequence.rs
+++ src/solutions/longest_collatz_sequence.rs
@@ -15,12 +15,15 @@ fn get_collatz_length(collatz_lengths: &mut Vec<i32>, num: usize) -> i32 {
     for count in 1.. {
         // Keep transforming this number until it can be used as an index into
         // the cache.
-        num_next = if num_next % 2 == 0 {num_next / 2} else {3*num_next+1};
+        num_next = if num_next % 2 == 0 {
+            num_next / 2
+        } else {
+            3 * num_next + 1
+        };
         if num_next >= collatz_lengths.len() as u64 {
             continue;
         }
-        let num_next = num_next as usize;
-        collatz_lengths[num] = count + get_collatz_length(collatz_lengths, num_next);
+        collatz_lengths[num] = count + get_collatz_length(collatz_lengths, num_next as usize);
         return collatz_lengths[num];
     }
     unreachable!();
