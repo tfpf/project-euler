e488c9eebb5a19ba06329797453d256093bdf2ac
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-09-21 12:00:31 +0530

    Formatted

--- src/solutions/largest_prime_factor.rs
+++ src/solutions/largest_prime_factor.rs
@@ -3,22 +3,27 @@ use crate::utils;
 /// Determine the largest prime factor of the given number.
 ///
 /// * `num`
-fn largest_prime_factor(mut num: i64) ->i64 {
+fn largest_prime_factor(mut num: i64) -> i64 {
     // Not divisible by 2, 3 or 5. Look for other factors.
-    utils::PotentialPrimes::new(num).map_while(|potential_prime| {
-        if potential_prime > num {None} else if num % potential_prime != 0 {
-            // Dummy result so that the loop continues looking for factors and
-            // does not terminate.
-            Some(0)
-        }else{
-            // Found a potential prime factor. Eliminate it. Doing this ensures
-            // that all factors found are actually prime factors.
-            while num % potential_prime == 0 {
-                num /= potential_prime;
+    utils::PotentialPrimes::new(num)
+        .map_while(|potential_prime| {
+            if potential_prime > num {
+                None
+            } else if num % potential_prime != 0 {
+                // Dummy result so that the loop continues looking for factors and
+                // does not terminate.
+                Some(0)
+            } else {
+                // Found a potential prime factor. Eliminate it. Doing this ensures
+                // that all factors found are actually prime factors.
+                while num % potential_prime == 0 {
+                    num /= potential_prime;
+                }
+                Some(potential_prime)
             }
-            Some(potential_prime)
-        }
-    }).max().unwrap()
+        })
+        .max()
+        .unwrap()
 }
 
 pub fn solve() -> i64 {
