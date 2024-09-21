abe0338f632c176317bd1e0f51fb057eef94fff4
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2024-09-21 12:10:46 +0530

    `PotentialPrimes` optimisation (#163)
    * `PotentialPrimes`: generate starting from 7
      * Removed `PrimeDivisors`, which required `PotentialPrimes` and was used in only one solution.
      * This change reduced the running time of solution 27 from 7 ms to 4 ms because of the related change in `is_prime_td`.
    * Formatted

--- src/utils/iterators/potential_primes.rs
+++ src/utils/iterators/potential_primes.rs
@@ -1,5 +1,5 @@
-/// Potential prime numbers. Generates some small prime numbers and numbers
-/// coprime to 30. Used for wheel factorisation with 2, 3 and 5.
+/// Potential prime numbers. Generates numbers coprime to 30, starting from 7.
+/// Used for wheel factorisation with 2, 3 and 5.
 pub struct PotentialPrimes {
     limit: i64,
     num: i64,
@@ -11,7 +11,7 @@ impl PotentialPrimes {
         PotentialPrimes {
             limit,
             num: 1,
-            offset: [4, 2, 4, 2, 4, 6, 2, 6].into_iter().cycle(),
+            offset: [6, 4, 2, 4, 2, 4, 6, 2].into_iter().cycle(),
         }
     }
 }
@@ -19,12 +19,7 @@ impl PotentialPrimes {
 impl Iterator for PotentialPrimes {
     type Item = i64;
     fn next(&mut self) -> Option<i64> {
-        self.num += match self.num {
-            1 => 1,
-            2 => 1,
-            3 | 5 => 2,
-            _ => self.offset.next().unwrap(),
-        };
+        self.num += self.offset.next().unwrap();
         if self.num > self.limit {
             None
         } else {
